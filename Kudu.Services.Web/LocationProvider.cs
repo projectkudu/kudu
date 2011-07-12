8146a26e505c74179f82cd85a55353f116caacdb
David Fowler <davidfowl@gmail.com>
2011-07-12 10:31:02 -0700

    Moved repository root folder from under bin to avoid app domain restarts.

--- Kudu.Services.Web/LocationProvider.cs
+++ Kudu.Services.Web/LocationProvider.cs
@@ -1,27 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.IO;
 using System.Web;
-using System.Web.Routing;
-using System.IO;
 
 namespace Kudu.Services.Web {
     public class LocationProvider : ILocationProvider {
         public string RepositoryRoot {
             get {
-                return Path.Combine(GetRootPath(), @"repository");
+                string path = Path.Combine(GetRootPath(), @"repository");
+                EnsureDirectory(path);
+                return path;
             }
         }
 
         private string GetRootPath() {
-            // Temporary path (under bin folder so we don't need to ignore it in source control)
-            string path = Path.Combine(HttpRuntime.BinDirectory, "_root");
+            return Path.Combine(HttpRuntime.AppDomainAppPath, "_root");
+        }
 
+        private void EnsureDirectory(string path) {
             if (!Directory.Exists(path)) {
                 Directory.CreateDirectory(path);
             }
-
-            return path;
         }
     }
 }
