e7eafd879f08f015ed264140a8d638eec7f7ddf7
David Fowler <davidfowl@gmail.com>
2011-07-14 21:40:17 -0700

    Skip certain files in the PhysicalFileSystem.

--- Kudu.Core/Editor/PhysicalFileSystem.cs
+++ Kudu.Core/Editor/PhysicalFileSystem.cs
@@ -3,13 +3,13 @@ using System.IO;
 using System.Linq;
 
 namespace Kudu.Core.Editor {
-    public class PhysicalFileSystem : IFileSystem {        
+    public class PhysicalFileSystem : IFileSystem {
         private readonly string _root;
 
         public PhysicalFileSystem(string root) {
             _root = root;
         }
-        
+
         private string GetFullPath(string path) {
             string normalizedPath = path.Replace(Path.AltDirectorySeparatorChar, Path.DirectorySeparatorChar).TrimStart(Path.DirectorySeparatorChar);
             return Path.Combine(_root, path);
@@ -20,10 +20,27 @@ namespace Kudu.Core.Editor {
         }
 
         public virtual IEnumerable<string> GetFiles() {
-            return Directory.GetFiles(_root, "*.*", SearchOption.AllDirectories)
-                            .Select(MakeRelative);
+            var directory = new DirectoryInfo(_root);
+            return GetFiles(directory);
         }
- 
+
+        private IEnumerable<string> GetFiles(DirectoryInfo directory) {
+            if (directory.Name.StartsWith(".") ||
+                directory.Attributes.HasFlag(FileAttributes.Hidden)) {
+                yield break;
+            }
+
+            foreach (var file in directory.EnumerateFiles()) {
+                yield return MakeRelative(file.FullName);
+            }
+
+            foreach (var subDirectory in directory.EnumerateDirectories()) {
+                foreach (var file in GetFiles(subDirectory)) {
+                    yield return file;
+                }
+            }
+        }
+
         public void WriteAllText(string path, string text) {
             File.WriteAllText(GetFullPath(path), text);
         }
@@ -31,7 +48,7 @@ namespace Kudu.Core.Editor {
         public void Delete(string path) {
             File.Delete(GetFullPath(path));
         }
-        
+
         protected string MakeRelative(string path) {
             return path.Substring(_root.Length).TrimStart(Path.DirectorySeparatorChar).Replace(Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar);
         }
