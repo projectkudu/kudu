a3a294e8ac58f60de55e31a0f756c5d9a86e6b2c
Haacked <haacked@gmail.com>
2011-07-18 16:48:32 -0700

    Changed forbidden result to use built-in type

--- Kudu.Services/GitServer/ForbiddenResult.cs
+++ Kudu.Services/GitServer/ForbiddenResult.cs
@@ -21,13 +21,11 @@
 #endregion
 
 namespace Kudu.Services.GitServer {
-	using System.Web.Mvc;
+    using System.Net;
+    using System.Web.Mvc;
 
-	public class ForbiddenResult : ActionResult {
-		public override void ExecuteResult(ControllerContext context) {
-			context.HttpContext.Response.StatusCode = 403;
-			context.HttpContext.Response.ContentType = "text/plain";
-			context.HttpContext.Response.Write("Forbidden");
-		}
-	}
+    public class ForbiddenResult : HttpStatusCodeResult {
+        public ForbiddenResult() : base((int)HttpStatusCode.Forbidden, "Forbidden") {
+        }
+    }
 }
