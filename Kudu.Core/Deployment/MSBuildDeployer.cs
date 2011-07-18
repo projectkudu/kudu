dc80f9f85d81d346f6d1c151812eb165979593c0
David Fowler <davidfowl@gmail.com>
2011-07-18 01:25:16 -0700

    Build with SolutionDir variable

--- Kudu.Core/Deployment/MSBuildDeployer.cs
+++ Kudu.Core/Deployment/MSBuildDeployer.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Linq;
 using Kudu.Core.Infrastructure;
 using SystemEnvironment = System.Environment;
@@ -13,12 +14,32 @@ namespace Kudu.Core.Deployment {
             _msbuildExe = new Executable(ResolveMSBuildPath(), environment.RepositoryPath);
         }
 
-        public void Deploy(string id) {
+        public void Deploy(string id) {            
             // TODO: Have some convention (or setting to determine the deployment project if more than one)
             string projectFile = _environment.GetWebApplicationProjects().FirstOrDefault();
 
+            // Locate the solution directory
+            string solutionDir = GetSolutionDir(projectFile);
+
             // REVIEW: Should we use the msbuild API?
-            _msbuildExe.Execute(@"""{0}"" /t:pipelinePreDeployCopyAllFilesToOneFolder /p:_PackageTempDir={1};AutoParameterizationWebConfigConnectionStrings=false", projectFile, _environment.DeploymentPath);
+            _msbuildExe.Execute(@"""{0}"" /t:pipelinePreDeployCopyAllFilesToOneFolder /p:_PackageTempDir={1};AutoParameterizationWebConfigConnectionStrings=false;SolutionDir={2}", projectFile, _environment.DeploymentPath, solutionDir);
+        }
+
+        private string GetSolutionDir(string projectPath) {
+            string path = projectPath;
+
+            while (!_environment.RepositoryPath.Equals(path, StringComparison.OrdinalIgnoreCase)) {
+                path = Path.GetDirectoryName(path);
+                if (Directory.EnumerateFiles(path, "*.sln").Any()) {
+                    // TODO: Ensure that this project is in this solution
+
+                    // Add the trailing slash
+                    return path + "//";
+                }
+            }
+
+            // This is the default value for SolutionDir when it's not defined in a build
+            return "*Undefined*";
         }
 
         private string ResolveMSBuildPath() {
