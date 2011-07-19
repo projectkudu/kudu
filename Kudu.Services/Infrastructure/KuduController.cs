b25b2052f725dc51f6f630ffe7531e59735473a0
Haacked <haacked@gmail.com>
2011-07-18 20:42:29 -0700

    Adding files forgotten in last commit

---
+++ Kudu.Services/Infrastructure/KuduController.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+using System.Web.Mvc;
+
+namespace Kudu.Services.Infrastructure {
+    public class KuduController : Controller {
+        protected override IActionInvoker CreateActionInvoker() {
+            return new KuduActionInvoker();
+        }
+
+        protected override JsonResult Json(object data, string contentType, Encoding contentEncoding, JsonRequestBehavior behavior) {
+            return new KuduJsonResult {
+                Data = data,
+                ContentType = contentType,
+                ContentEncoding = contentEncoding,
+                JsonRequestBehavior = behavior,
+                MaxJsonLength = int.MaxValue
+            };
+        }
+    }
+}
