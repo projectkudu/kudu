533d060c1a41430f752d74fe86dc6501422a9835
David Fowler <davidfowl@gmail.com>
2011-07-30 14:35:42 -0700

    Cleanup solution parsing code

--- Kudu.Core/Infrastructure/VsSolution.cs
+++ Kudu.Core/Infrastructure/VsSolution.cs
@@ -6,21 +6,23 @@ using System.Reflection;
 
 namespace Kudu.Core.Infrastructure {
     public class VsSolution {
-        //internal class SolutionParser
-        //Name: Microsoft.Build.Construction.SolutionParser
-        //Assembly: Microsoft.Build, Version=4.0.0.0
+        // internal class SolutionParser
+        // Name: Microsoft.Build.Construction.SolutionParser
+        // Assembly: Microsoft.Build, Version=4.0.0.0
+        private const string SolutionParserTypeName = "Microsoft.Build.Construction.SolutionParser, Microsoft.Build, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a";
 
         private static readonly Type _solutionParser;
-        private static readonly PropertyInfo _solutionParser_solutionReader;
-        private static readonly MethodInfo _solutionParser_parseSolution;
-        private static readonly PropertyInfo _solutionParser_projects;
+        private static readonly PropertyInfo solutionReaderProperty;
+        private static readonly MethodInfo parseSolutionMethod;
+        private static readonly PropertyInfo projectsProperty;
 
         static VsSolution() {
-            _solutionParser = Type.GetType("Microsoft.Build.Construction.SolutionParser, Microsoft.Build, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a", false, false);
+            _solutionParser = Type.GetType(SolutionParserTypeName, throwOnError: false, ignoreCase: false);
+
             if (_solutionParser != null) {
-                _solutionParser_solutionReader = _solutionParser.GetProperty("SolutionReader", BindingFlags.NonPublic | BindingFlags.Instance);
-                _solutionParser_projects = _solutionParser.GetProperty("Projects", BindingFlags.NonPublic | BindingFlags.Instance);
-                _solutionParser_parseSolution = _solutionParser.GetMethod("ParseSolution", BindingFlags.NonPublic | BindingFlags.Instance);
+                solutionReaderProperty = ReflectionUtility.GetInternalProperty(_solutionParser, "SolutionReader");
+                projectsProperty = ReflectionUtility.GetInternalProperty(_solutionParser, "Projects");
+                parseSolutionMethod = ReflectionUtility.GetInternalMethod(_solutionParser, "ParseSolution");
             }
         }
 
@@ -32,25 +34,35 @@ namespace Kudu.Core.Infrastructure {
                 throw new InvalidOperationException("Can not find type 'Microsoft.Build.Construction.SolutionParser' are you missing a assembly reference to 'Microsoft.Build.dll'?");
             }
 
-            var solutionParser = _solutionParser.GetConstructors(BindingFlags.Instance | BindingFlags.NonPublic).First().Invoke(null);
+            var solutionParser = GetSolutionParserInstance();
+
             using (var streamReader = new StreamReader(solutionPath)) {
-                _solutionParser_solutionReader.SetValue(solutionParser, streamReader, null);
-                _solutionParser_parseSolution.Invoke(solutionParser, null);
+                solutionReaderProperty.SetValue(solutionParser, streamReader);
+                parseSolutionMethod.Invoke(solutionParser, null);
             }
 
             var projects = new List<VsSolutionProject>();
-            var array = (Array)_solutionParser_projects.GetValue(solutionParser, null);
-            for (int i = 0; i < array.Length; i++) {
-                projects.Add(new VsSolutionProject(solutionPath, array.GetValue(i)));
-            }
+            var projectsArray = projectsProperty.GetValue<object[]>(solutionParser);
 
+            foreach (var project in projectsArray) {
+                projects.Add(new VsSolutionProject(solutionPath, project));
+            }
+            
             Path = solutionPath;
             Projects = projects;
+        }
+
+        private static object GetSolutionParserInstance() {
+            // Get the constructor for Solution parser
+            var ctor = _solutionParser.GetConstructors(BindingFlags.Instance | BindingFlags.NonPublic).First();
+
+            // Create an instance of the solution parser
+            return ctor.Invoke(null);
         }
 
         public static IEnumerable<VsSolution> GetSolutions(string path) {
             return from solutionFile in Directory.EnumerateFiles(path, "*.sln", SearchOption.AllDirectories)
                    select new VsSolution(solutionFile);
         }
-    }    
+    }
 }
--- Kudu.Core/Infrastructure/VsSolutionProject.cs
+++ Kudu.Core/Infrastructure/VsSolutionProject.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
@@ -8,38 +9,52 @@ using System.Xml.Linq;
 namespace Kudu.Core.Infrastructure {
     [DebuggerDisplay("{ProjectName}")]
     public class VsSolutionProject {
+        private const string ProjectInSolutionTypeName = "Microsoft.Build.Construction.ProjectInSolution, Microsoft.Build, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a";
+
         private static readonly Guid _wapGuid = new Guid("349c5851-65df-11da-9384-00065b846f21");
 
-        private static readonly Type _projectInSolution;
-        private static readonly PropertyInfo _projectInSolution_ProjectName;
-        private static readonly PropertyInfo _projectInSolution_RelativePath;
-        private static readonly PropertyInfo _projectInSolution_ProjectType;
+        private static readonly Type _projectInSolutionType;
+        private static readonly PropertyInfo _projectNameProperty;
+        private static readonly PropertyInfo _relativePathProperty;
+        private static readonly PropertyInfo _projectTypeProperty;
 
         static VsSolutionProject() {
-            _projectInSolution = Type.GetType("Microsoft.Build.Construction.ProjectInSolution, Microsoft.Build, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a", throwOnError: false, ignoreCase: false);
-            if (_projectInSolution != null) {
-                _projectInSolution_ProjectName = _projectInSolution.GetProperty("ProjectName", BindingFlags.NonPublic | BindingFlags.Instance);
-                _projectInSolution_RelativePath = _projectInSolution.GetProperty("RelativePath", BindingFlags.NonPublic | BindingFlags.Instance);
-                _projectInSolution_ProjectType = _projectInSolution.GetProperty("ProjectType", BindingFlags.NonPublic | BindingFlags.Instance);
+            _projectInSolutionType = Type.GetType(ProjectInSolutionTypeName, throwOnError: false, ignoreCase: false);
+
+            if (_projectInSolutionType != null) {
+                _projectNameProperty = ReflectionUtility.GetInternalProperty(_projectInSolutionType, "ProjectName");
+                _relativePathProperty = ReflectionUtility.GetInternalProperty(_projectInSolutionType, "RelativePath");
+                _projectTypeProperty = ReflectionUtility.GetInternalProperty(_projectInSolutionType, "ProjectType");
             }
         }
 
+        public IEnumerable<Guid> ProjectTypeGuids { get; set; }
         public string ProjectName { get; private set; }
         public string AbsolutePath { get; private set; }
         public bool IsWebSite { get; private set; }
         public bool IsWap { get; set; }
 
-        public VsSolutionProject(string solutionPath, object solutionProject) {
-            ProjectName = _projectInSolution_ProjectName.GetValue(solutionProject, null) as string;
-            string relativePath = _projectInSolution_RelativePath.GetValue(solutionProject, null) as string;
+        public VsSolutionProject(string solutionPath, object project) {
+            ProjectName = _projectNameProperty.GetValue<string>(project);
+            var relativePath = _relativePathProperty.GetValue<string>(project);
+            var projectType = _projectTypeProperty.GetValue<SolutionProjectType>(project);
+
             AbsolutePath = Path.Combine(Path.GetDirectoryName(solutionPath), relativePath);
-            var projectType = (int)_projectInSolution_ProjectType.GetValue(solutionProject, null);
-            IsWebSite = projectType == 3;
-            IsWap = projectType == 1 && IsProjectFileWap(AbsolutePath);
+            IsWebSite = projectType == SolutionProjectType.WebProject;
+
+            if (projectType == SolutionProjectType.KnownToBeMSBuildFormat) {
+                // If the project is an msbuild project then extra the project type guids
+                ProjectTypeGuids = GetProjectTypeGuids(AbsolutePath);
+
+                // Check if it's a wap
+                IsWap = ProjectTypeGuids.Contains(_wapGuid);
+            }
+            else {
+                ProjectTypeGuids = Enumerable.Empty<Guid>();
+            }
         }
 
-
-        private static bool IsProjectFileWap(string path) {
+        private static IEnumerable<Guid> GetProjectTypeGuids(string path) {
             var document = XDocument.Parse(File.ReadAllText(path));
 
             var guids = from propertyGroup in document.Root.Elements(GetName("PropertyGroup"))
@@ -47,12 +62,21 @@ namespace Kudu.Core.Infrastructure {
                         where projectTypeGuids != null
                         from guid in projectTypeGuids.Value.Split(';')
                         select new Guid(guid.Trim('{', '}'));
-
-            return guids.Contains(_wapGuid);
+            return guids;
         }
 
         private static XName GetName(string name) {
             return XName.Get(name, "http://schemas.microsoft.com/developer/msbuild/2003");
+        }
+
+        // Microsoft.Build.Construction.SolutionProjectType
+        private enum SolutionProjectType {
+            Unknown,
+            KnownToBeMSBuildFormat,
+            SolutionFolder,
+            WebProject,
+            WebDeploymentProject,
+            EtpSubProject,
         }
     }
 }
