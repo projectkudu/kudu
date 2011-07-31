79e400c8d3f4c01f4fc08f8f854c25f944543091
David Fowler <davidfowl@gmail.com>
2011-07-31 14:23:19 -0700

    Fixed formatting

--- Kudu.Services.Web/App_Start/NinjectServices.cs
+++ Kudu.Services.Web/App_Start/NinjectServices.cs
@@ -7,10 +7,10 @@ using Kudu.Core.SourceControl;
 using Kudu.Core.SourceControl.Git;
 using Kudu.Core.SourceControl.Hg;
 using Kudu.Services.Authorization;
+using Kudu.Services.Deployment;
 using Microsoft.Web.Infrastructure.DynamicModuleHelper;
 using Ninject;
 using Ninject.Web.Mvc;
-using Kudu.Services.Deployment;
 
 [assembly: WebActivator.PreApplicationStartMethod(typeof(Kudu.Services.Web.App_Start.NinjectServices), "Start")]
 [assembly: WebActivator.ApplicationShutdownMethodAttribute(typeof(Kudu.Services.Web.App_Start.NinjectServices), "Stop")]
@@ -55,7 +55,7 @@ namespace Kudu.Services.Web.App_Start {
         /// <param name="kernel">The kernel.</param>
         private static void RegisterServices(IKernel kernel) {
             kernel.Bind<IEnvironment>().ToMethod(_ => GetEnvironment()).InSingletonScope();
-                        kernel.Bind<IDeploymentNotifier>().To<ConnectionNotifier>().InSingletonScope();
+            kernel.Bind<IDeploymentNotifier>().To<ConnectionNotifier>().InSingletonScope();
             kernel.Bind<IRepositoryManager>().ToMethod(context => new RepositoryManager(context.Kernel.Get<IEnvironment>().RepositoryPath));
             kernel.Bind<ISiteBuilderFactory>().To<SiteBuilderFactory>();
             kernel.Bind<IDeploymentManager>().To<DeploymentManager>();
