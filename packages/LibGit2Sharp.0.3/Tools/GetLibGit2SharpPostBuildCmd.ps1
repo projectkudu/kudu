f41af7bf194ec6edadf86e37c66dfd612607c85b
David Fowler <davidfowl@gmail.com>
2011-08-03 21:28:00 -0700

    Updated packages

--- packages/LibGit2Sharp.0.3/Tools/GetLibGit2SharpPostBuildCmd.ps1
+++
@@ -1,8 +0,0 @@
-$solutionDir = [System.IO.Path]::GetDirectoryName($dte.Solution.FullName) + "\"
-$path = $installPath.Replace($solutionDir, "`$(SolutionDir)")
-
-$NativeAssembliesDir = Join-Path $path "NativeBinaries"
-$x86 = $(Join-Path $NativeAssembliesDir "x86\*.*")
-
-$LibGit2SharpPostBuildCmd = "
-xcopy /s /y `"$x86`" `"`$(TargetDir)`""
