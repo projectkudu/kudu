6f5ddc82cad4cedefe548b51dde0a1f4820555ca
David Fowler <davidfowl@gmail.com>
2011-09-16 02:09:13 -0700

    Use sql server compact

---
+++ Kudu.Web/App_Start/EntityFramework.SqlServerCompact.cs
@@ -0,0 +1,12 @@
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+
+[assembly: WebActivator.PreApplicationStartMethod(typeof(Kudu.Web.App_Start.EntityFramework_SqlServerCompact), "Start")]
+
+namespace Kudu.Web.App_Start {
+    public static class EntityFramework_SqlServerCompact {
+        public static void Start() {
+            Database.DefaultConnectionFactory = new SqlCeConnectionFactory("System.Data.SqlServerCe.4.0");
+        }
+    }
+}
