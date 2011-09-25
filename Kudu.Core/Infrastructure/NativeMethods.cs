bd4bc1e9fdd0f6b7766f4d0dc0abbbac8916d1fa
David Fowler <davidfowl@gmail.com>
2011-09-25 16:54:12 -0700

    Removed MappedDrive class since we use an IDictionary

--- Kudu.Core/Infrastructure/NativeMethods.cs
+++ Kudu.Core/Infrastructure/NativeMethods.cs
@@ -5,10 +5,5 @@ namespace Kudu.Core.Infrastructure {
     public static class NativeMethods {
         [DllImport("mpr.dll", CharSet = CharSet.Unicode, SetLastError = true)]
         public static extern int WNetGetConnection([MarshalAs(UnmanagedType.LPTStr)] string localName, [MarshalAs(UnmanagedType.LPTStr)] StringBuilder remoteName, ref int length);
-
-        public class MappedDrive {
-            public string LocalDriveLetter { get; set; }
-            public string UncPath { get; set; }
-        }
     }
 }
