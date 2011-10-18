8f6a5ab20b80938130119443e7885763cadf31b8
David Fowler <davidfowl@gmail.com>
2011-10-18 08:52:09 -0700

    Added loader component

---
+++ Kudu.Web/Components/Loader/loader.js
@@ -0,0 +1,32 @@
+﻿(function ($) {
+    /// <param name="$" type="jQuery" />
+    "use strict"
+
+    var token = 0;
+
+    $.fn.loader = function () {
+        var $this = $(this),
+            that = null;
+
+        that = {
+            show: function (value) {
+                token++;
+                $this.data('token', token);
+                $this.html(value);
+                $this.attr('class', 'icon icon-loading');
+                $this.show();
+                return token;
+            },
+            hide: function () {
+                window.setTimeout(function () {
+                    if ($this.data('token') == token) {
+                        $this.fadeOut('slow');
+                    }
+                }, 100);
+            }
+        };
+
+        return that;
+    }
+
+})(jQuery);
