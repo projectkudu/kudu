5b6a519e71d4dc4c98d79dcca94d3a831f0b68c5
David Fowler <davidfowl@gmail.com>
2011-10-24 20:20:47 -0700

    Run unit tests on build.

--- build.cmd
+++ build.cmd
@@ -3,4 +3,4 @@ set config=%1
 if "%config%" == "" (
    set config=debug
 )
-%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild Kudu.sln /p:Configuration="%config%";MSBuildExtensionsPath32=..\Kudu.Services.Web\msbuild /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild Build\Build.proj /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
