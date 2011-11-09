42fe76dcd43f90cee261cae03d9cdcc432e2bf2c
David Fowler <davidfowl@gmail.com>
2011-11-09 02:00:57 -0800

    Made package for Kudu.SignalR. Generate packages in one folder for convenience. Use shared property info.

---
+++ Common/CommonAssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyCompany("Outercurve Foundation")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyConfiguration("")]
+
+[assembly: AssemblyVersion("0.5.*")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- Kudu.Core/Properties/AssemblyInfo.cs
+++ Kudu.Core/Properties/AssemblyInfo.cs
@@ -4,10 +4,5 @@ using System.Runtime.CompilerServices;
 
 [assembly: AssemblyTitle("Kudu.Core")]
 [assembly: AssemblyDescription("Core APIs for Kudu.")]
-[assembly: AssemblyCompany("Outercurve Foundation")]
 [assembly: AssemblyProduct("Kudu.Core")]
-
-[assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-
 [assembly: InternalsVisibleTo("Kudu.Core.Test")]
