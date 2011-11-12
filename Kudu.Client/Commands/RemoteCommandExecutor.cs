projectkudu/kudu

commit da3d1ca84419349f659782f1e459356d95e9a9b5

Refactored remote clients with base class

Kudu.Client/Commands/RemoteCommandExecutor.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Net;
 using System.Net.Http;
 using Kudu.Client.Infrastructure;
 using Kudu.Core.Commands;
@@ -9,17 +8,13 @@ using SignalR.Client;
 
 namespace Kudu.Client.Commands
 {
-    public class RemoteCommandExecutor : ICommandExecutor, IKuduClientCredentials
+    public class RemoteCommandExecutor : KuduRemoteClientBase, ICommandExecutor
     {
-        private readonly HttpClient _client;
         private readonly Connection _connection;
-        private ICredentials _credentials;
 
         public RemoteCommandExecutor(string serviceUrl)
+            : base(serviceUrl)
         {
-            serviceUrl = UrlUtility.EnsureTrailingSlash(serviceUrl);
-            _client = HttpClientHelper.Create(serviceUrl);
-
             _connection = new Connection(serviceUrl + "status");
             _connection.Received += data =>
             {
@@ -31,19 +26,6 @@ namespace Kudu.Client.Commands
             };
 
             _connection.Start();
-        }
-
-        public ICredentials Credentials
-        {
-            get
-            {
-                return this._credentials;
-            }
-            set
-            {
-                this._credentials = value;
-                this._client.SetClientCredentials(this._credentials);
-            }
         }
 
         public event Action<CommandEvent> CommandEvent;

Kudu.Client/Deployment/RemoteDeploymentManager.cs
@@ -10,20 +10,15 @@ using SignalR.Client;
 
 namespace Kudu.Client.Deployment
 {
-    public class RemoteDeploymentManager : IDeploymentManager, IKuduClientCredentials
+    public class RemoteDeploymentManager : KuduRemoteClientBase, IDeploymentManager
     {
-        private readonly HttpClient _client;
         private readonly Connection _connection;
-        private ICredentials _credentials;
 
         public event Action<DeployResult> StatusChanged;
 
-
         public RemoteDeploymentManager(string serviceUrl)
+            : base(serviceUrl)
         {
-            serviceUrl = UrlUtility.EnsureTrailingSlash(serviceUrl);
-            _client = HttpClientHelper.Create(serviceUrl);
-
             // Raise the event when data comes in
             _connection = new Connection(serviceUrl + "status");
             _connection.Received += data =>
@@ -56,19 +51,6 @@ namespace Kudu.Client.Deployment
             };
 
             _connection.Start().Wait();
-        }
-
-        public ICredentials Credentials
-        {
-            get
-            {
-                return this._credentials;
-            }
-            set
-            {
-                this._credentials = value;
-                this._client.SetClientCredentials(this._credentials);
-            }
         }
 
         public bool IsActive

Kudu.Client/Infrastructure/KuduRemoteClientBase.cs
@@ -1,0 +1,30 @@
+﻿using System.Net;
+using System.Net.Http;
+
+namespace Kudu.Client.Infrastructure
+{
+    public abstract class KuduRemoteClientBase
+    {
+        protected readonly HttpClient _client;
+        private ICredentials _credentials;
+
+        public KuduRemoteClientBase(string serviceUrl)
+        {
+            serviceUrl = UrlUtility.EnsureTrailingSlash(serviceUrl);
+            _client = HttpClientHelper.Create(serviceUrl);
+        }
+
+        public ICredentials Credentials
+        {
+            get
+            {
+                return _credentials;
+            }
+            set
+            {
+                _credentials = value;
+                _client.SetClientCredentials(_credentials);
+            }
+        }
+    }
+}

Kudu.Client/Editor/RemoteProjectSystem.cs
@@ -1,20 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Net;
-using System.Net.Http;
 using Kudu.Client.Infrastructure;
 using Kudu.Core.Editor;
 
 namespace Kudu.Client.Editor
 {
-    public class RemoteProjectSystem : IProjectSystem, IKuduClientCredentials
+    public class RemoteProjectSystem : KuduRemoteClientBase, IProjectSystem
     {
-        private readonly HttpClient _client;
-        private ICredentials _credentials;
-
         public RemoteProjectSystem(string serviceUrl)
+            :base(serviceUrl)
         {
-            _client = HttpClientHelper.Create(serviceUrl);
         }
 
         public string ReadAllText(string path)
@@ -25,19 +20,6 @@ namespace Kudu.Client.Editor
                           .EnsureSuccessful()
                           .Content
                           .ReadAsString();
-        }
-
-        public ICredentials Credentials
-        {
-            get
-            {
-                return this._credentials;
-            }
-            set
-            {
-                this._credentials = value;
-                this._client.SetClientCredentials(this._credentials);
-            }
         }
 
         public Project GetProject()

Kudu.Client/Infrastructure/IKuduClientCredentials.cs
@@ -1,15 +1,0 @@
-﻿// -----------------------------------------------------------------------
-// <copyright file="IKuduCredentials.cs" company="Microsoft">
-// TODO: Update copyright text.
-// </copyright>
-// -----------------------------------------------------------------------
-
-namespace Kudu.Client.Infrastructure
-{
-    using System.Net;
-
-    public interface IKuduClientCredentials
-    {
-        ICredentials Credentials { get; set; }
-    }
-}

Kudu.Client/Kudu.Client.csproj
@@ -79,7 +79,7 @@
     </Compile>
     <Compile Include="Infrastructure\HttpClientExtensions.cs" />
     <Compile Include="Infrastructure\HttpClientHelper.cs" />
-    <Compile Include="Infrastructure\IKuduClientCredentials.cs" />
+    <Compile Include="Infrastructure\KuduRemoteClientBase.cs" />
     <Compile Include="Infrastructure\UrlUtility.cs" />
     <Compile Include="Properties\AssemblyInfo.cs" />
     <Compile Include="Commands\RemoteCommandExecutor.cs" />

Kudu.Client/Settings/RemoteDeploymentSettingsManager.cs
@@ -1,34 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Net;
-using System.Net.Http;
+﻿using System.Collections.Generic;
 using Kudu.Client.Infrastructure;
 using Kudu.Core.Deployment;
 
 namespace Kudu.Client.Deployment
 {
-    public class RemoteDeploymentSettingsManager : IDeploymentSettingsManager, IKuduClientCredentials
+    public class RemoteDeploymentSettingsManager : KuduRemoteClientBase, IDeploymentSettingsManager
     {
-        private readonly HttpClient _client;
-        private ICredentials _credentials;
-
         public RemoteDeploymentSettingsManager(string serviceUrl)
+            :base(serviceUrl)
         {
-            serviceUrl = UrlUtility.EnsureTrailingSlash(serviceUrl);
-            _client = HttpClientHelper.Create(serviceUrl);
-        }
-
-        public ICredentials Credentials
-        {
-            get
-            {
-                return this._credentials;
-            }
-            set
-            {
-                this._credentials = value;
-                this._client.SetClientCredentials(this._credentials);
-            }
         }
 
         public IEnumerable<DeploymentSetting> GetAppSettings()

Kudu.Client/SourceControl/RemoteRepository.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Net;
 using System.Net.Http;
 using Kudu.Client.Infrastructure;
 using Kudu.Core.SourceControl;
@@ -8,27 +7,11 @@ using Newtonsoft.Json;
 
 namespace Kudu.Client.SourceControl
 {
-    public class RemoteRepository : IRepository, IKuduClientCredentials
+    public class RemoteRepository : KuduRemoteClientBase, IRepository
     {
-        private readonly HttpClient _client;
-        private ICredentials _credentials;
-
         public RemoteRepository(string serviceUrl)
+            :base(serviceUrl)
         {
-            _client = HttpClientHelper.Create(serviceUrl);
-        }
-
-        public ICredentials Credentials
-        {
-            get
-            {
-                return this._credentials;
-            }
-            set
-            {
-                this._credentials = value;
-                this._client.SetClientCredentials(this._credentials);
-            }
         }
 
         public string CurrentId

commit d7b7b557ea60f8ff22c96d7fd0217c8ef609db9c

Fixed issue caused by da3d1ca84. Use normalized service url in services.

Kudu.Client/Commands/RemoteCommandExecutor.cs
@@ -15,7 +15,7 @@ namespace Kudu.Client.Commands
         public RemoteCommandExecutor(string serviceUrl)
             : base(serviceUrl)
         {
-            _connection = new Connection(serviceUrl + "status");
+            _connection = new Connection(ServiceUrl + "status");
             _connection.Received += data =>
             {
                 if (CommandEvent != null)

Kudu.Client/Deployment/RemoteDeploymentManager.cs
@@ -20,7 +20,7 @@ namespace Kudu.Client.Deployment
             : base(serviceUrl)
         {
             // Raise the event when data comes in
-            _connection = new Connection(serviceUrl + "status");
+            _connection = new Connection(ServiceUrl + "status");
             _connection.Received += data =>
             {
                 if (StatusChanged != null)

Kudu.Client/Infrastructure/KuduRemoteClientBase.cs
@@ -10,9 +10,11 @@ namespace Kudu.Client.Infrastructure
 
         public KuduRemoteClientBase(string serviceUrl)
         {
-            serviceUrl = UrlUtility.EnsureTrailingSlash(serviceUrl);
-            _client = HttpClientHelper.Create(serviceUrl);
+            ServiceUrl = UrlUtility.EnsureTrailingSlash(serviceUrl);
+            _client = HttpClientHelper.Create(ServiceUrl);
         }
+
+        public string ServiceUrl { get; private set; }
 
         public ICredentials Credentials
         {