b59a6213f4612d3a068ac95c3a4a70dc709c9faa
David Fowler <davidfowl@gmail.com>
2012-01-10 13:41:06 -0800

    Map profiler route to profile instead of profiler

--- Kudu.Services.Web/Global.asax.cs
+++ Kudu.Services.Web/Global.asax.cs
@@ -66,7 +66,7 @@ namespace Kudu.Services
 
             if (AppSettings.ProfilingEnabled)
             {
-                MapServiceRoute<ProfilingService>("profiler", factory);
+                MapServiceRoute<ProfilingService>("profile", factory);
             }
         }
 
