826ca0afa0095c424908077eb56e8abb6c313bab
Raquel Almeida <raquel_soares@msn.com>
2012-02-28 10:46:26 -0800

    Adding Util files

---
+++ Kudu.TestHarness/HttpUtils.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Xml.Linq;
+using Ionic.Zip;
+
+namespace Kudu.TestHarness
+{
+    internal class HttpUtils
+    {
+        public static void WaitForSite(string siteUrl, int retries = 3, int delayBeforeRetry = 250)
+        {
+            var client = new HttpClient();
+
+            while (retries > 0)
+            {
+                try
+                {
+                    var response = client.GetAsync(siteUrl).Result;
+                    if (response.StatusCode == HttpStatusCode.OK)
+                    {
+                        break;
+                    }
+                }
+                catch
+                {
+                    if (retries == 0)
+                    {
+                        throw;
+                    }
+                }
+                retries--;
+                Thread.Sleep(delayBeforeRetry);
+            }
+        }        
+    }
+}
---
+++ Kudu.TestHarness/ZipUtils.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Xml.Linq;
+using Ionic.Zip;
+
+namespace Kudu.TestHarness
+{
+    internal class ZipUtils
+    {
+        public static void Unzip(string zipPath, string targetDir)
+        {
+            using (ZipFile zip = ZipFile.Read(zipPath))
+            {
+                foreach (ZipEntry file in zip)
+                {
+                    file.Extract(targetDir, ExtractExistingFileAction.OverwriteSilently);
+                }
+            }
+        }       
+    }
+}
