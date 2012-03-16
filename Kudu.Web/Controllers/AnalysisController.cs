5cb1088fc6c480c5189efb2f234b7e1955a04a8e
David Fowler <davidfowl@gmail.com>
2012-03-15 20:57:27 -0700

    Added tool for analyzing traces.

---
+++ Kudu.Web/Controllers/AnalysisController.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Mvc;
+using System.Xml.Linq;
+
+namespace Kudu.Web.Controllers
+{
+    public class AnalysisController : Controller
+    {
+        [HttpGet]
+        public ActionResult Trace()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ActionName("trace")]
+        public ActionResult PerformTrace()
+        {
+            var document = XDocument.Load(Request.Files[0].InputStream);
+            return View(document);
+        }
+    }
+}
