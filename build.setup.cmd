b588cc19db8831db4ddf8f6e4c869d4bfe46f2b7
David Fowler <davidfowl@gmail.com>
2012-03-27 02:09:30 -0700

    Merge targets file.

--- build.setup.cmd
+++ build.setup.cmd
@@ -3,4 +3,4 @@ set config=%1
 if "%config%" == "" (
    set config=debug
 )
-%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild Build\Build.Setup.proj /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild Build\Build.proj /t:BuildSetup /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
