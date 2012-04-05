2f27ec198f9ede47d9188ce0540184f2ecd1918e
David Fowler <davidfowl@gmail.com>
2012-04-04 19:12:02 -0700

    Added NullBuildPropertyProvider to IDeploymentManager injection.

--- Kudu.Services.Web/App_Start/NinjectServices.cs
+++ Kudu.Services.Web/App_Start/NinjectServices.cs
@@ -80,6 +80,7 @@ namespace Kudu.Services.Web.App_Start
             kernel.Bind<IServerConfiguration>().ToConstant(serverConfiguration);
             kernel.Bind<IFileSystem>().To<FileSystem>().InSingletonScope();
             kernel.Bind<RepositoryConfiguration>().ToConstant(gitConfiguration);
+            kernel.Bind<IBuildPropertyProvider>().ToConstant(NullBuildPropertyProvider.Instance);
 
             if (AppSettings.TraceEnabled)
             {
---
+++ Kudu.Services.Web/Services/NullBuildPropertyProvider.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Kudu.Core.Deployment;
+
+namespace Kudu.Services.Web.Services
+{
+    public class NullBuildPropertyProvider : IBuildPropertyProvider
+    {
+        private static readonly Dictionary<string, string> _empty = new Dictionary<string, string>();
+
+        public static readonly NullBuildPropertyProvider Instance = new NullBuildPropertyProvider();
+
+        public IDictionary<string, string> GetProperties()
+        {
+            return _empty;
+        }
+    }
+}
