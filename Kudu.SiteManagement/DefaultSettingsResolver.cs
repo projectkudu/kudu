02409e916bb7aff9f228166dc96dd7c476760bfe
Richard Price <richardprice@gmail.com>
2012-09-19 19:04:24 +0100

    Added supporting files for default hostname suffix support

---
+++ Kudu.SiteManagement/DefaultSettingsResolver.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace Kudu.SiteManagement
+{
+    public class DefaultSettingsResolver : ISettingsResolver
+    {
+        private readonly string _sitesBaseUrl;
+
+        public DefaultSettingsResolver(string sitesBaseUrl)
+        {
+            _sitesBaseUrl = sitesBaseUrl;
+        }
+
+        public string SitesBaseUrl
+        {
+            get
+            {
+                return _sitesBaseUrl;
+            }
+        }
+    }
+}
