032a8186d7378b1f1b7a8ca6850f015c683ba03a
Amit Apple <amitap@microsoft.com>
2012-10-10 11:03:20 -0700

    Updated unit tests in MsBuildSiteBuilderFacts to use PropertyData.

--- Kudu.Core.Test/Infrastructure/MsBuildSiteBuilderFacts.cs
+++ Kudu.Core.Test/Infrastructure/MsBuildSiteBuilderFacts.cs
@@ -4,39 +4,14 @@ using System.IO;
 using System.Threading.Tasks;
 using Kudu.Core.Deployment;
 using Xunit;
+using Xunit.Extensions;
 
 namespace Kudu.Core.Infrastructure.Test
 {
     public class MsBuildSiteBuilderFacts
     {
-        [Fact]
-        public void GetPropertyStringReturnsProperStringWith1Property()
-        {
-            Dictionary<string, string> properties = new Dictionary<string, string>();
-            properties["sanity"] = "sane";
-
-            this.RunGetPropertyStringScenario(properties, @"sanity=""sane""");
-        }
-
-        [Fact]
-        public void GetPropertyStringReturnsProperStringWith3Properties()
-        {
-            Dictionary<string, string> properties = new Dictionary<string, string>();
-            properties["sanity"] = "sane";
-            properties["sanity1"] = "sane1";
-            properties["sanity2"] = "sane2";
-
-            this.RunGetPropertyStringScenario(properties, @"sanity=""sane"";sanity1=""sane1"";sanity2=""sane2""");
-        }
-
-        [Fact]
-        public void GetPropertyStringReturnsProperStringWithNoPreperties()
-        {
-            Dictionary<string, string> properties = new Dictionary<string, string>();
-
-            this.RunGetPropertyStringScenario(properties, string.Empty);
-        }
-
+        [Theory]
+        [PropertyData("GetPropertyStringData")]
         private void RunGetPropertyStringScenario(Dictionary<string, string> properties, string expectedResult)
         {
             var buildPropertyProviderTest = new BuildPropertyProviderTest(properties);
@@ -44,6 +19,25 @@ namespace Kudu.Core.Infrastructure.Test
             var result = siteBuilder.GetPropertyStringForTest();
 
             Assert.Equal(expectedResult, result);
+        }
+
+        public static IEnumerable<object[]> GetPropertyStringData
+        {
+            get
+            {
+                Dictionary<string, string> properties = new Dictionary<string, string>();
+                properties["sanity"] = "sane";
+                yield return new object[] { properties, @"sanity=""sane""" };
+
+                properties = new Dictionary<string, string>();
+                properties["sanity"] = "sane";
+                properties["sanity1"] = "sane1";
+                properties["sanity2"] = "sane2";
+                yield return new object[] { properties, @"sanity=""sane"";sanity1=""sane1"";sanity2=""sane2""" };
+
+                properties = new Dictionary<string, string>();
+                yield return new object[] { properties, string.Empty };
+            }
         }
 
         private class MsBuildSiteBuilderTest : MsBuildSiteBuilder
