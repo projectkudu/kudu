1c8f37137a693db048f74599ced56cbae474f3ba
Remco Ros <r.ros@proscat.nl>
2012-11-07 03:32:58 +0100

    Add a 'serviceUrlBaseValue' appsetting. Works the same as urlBaseValue, for service sites.

--- Kudu.SiteManagement/DefaultSettingsResolver.cs
+++ Kudu.SiteManagement/DefaultSettingsResolver.cs
@@ -6,19 +6,24 @@ namespace Kudu.SiteManagement
     public class DefaultSettingsResolver : ISettingsResolver
     {
         private readonly string _sitesBaseUrl;
+        private readonly string _serviceSitesBaseUrl;
 
         public DefaultSettingsResolver()
-            : this(sitesBaseUrl: null)
+            : this(sitesBaseUrl: null, serviceSitesBaseUrl: null)
         {
         }
 
-        public DefaultSettingsResolver(string sitesBaseUrl)
+        public DefaultSettingsResolver(string sitesBaseUrl, string serviceSitesBaseUrl)
         {
             // Ensure the base url is normalised to not have a leading dot,
             // we will add this on later when joining the application name up
             if (sitesBaseUrl != null)
             {
                 _sitesBaseUrl = sitesBaseUrl.TrimStart('.');
+            }
+            if (serviceSitesBaseUrl != null)
+            {
+                _serviceSitesBaseUrl = serviceSitesBaseUrl.TrimStart('.');
             }
         }
 
@@ -28,6 +33,14 @@ namespace Kudu.SiteManagement
             {
                 return _sitesBaseUrl;
             }
+        }
+
+        public string ServiceSitesBaseUrl
+        {
+            get
+            {
+                return _serviceSitesBaseUrl;
+            }            
         }
     }
 }
--- Kudu.SiteManagement/ISettingsResolver.cs
+++ Kudu.SiteManagement/ISettingsResolver.cs
@@ -3,5 +3,7 @@
     public interface ISettingsResolver
     {
         string SitesBaseUrl { get; }
+        
+        string ServiceSitesBaseUrl { get; }
     }
 }
