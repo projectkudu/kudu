83d5cf41e58901e4db95fac7810a281649be5f14
Remco Ros <r.ros@proscat.nl>
2012-11-25 18:42:42 +0100

    first stab at more flexible deploy hook format parser

---
+++ Kudu.Services/GitServer/ServiceHookParser/Github.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Web;
+using Newtonsoft.Json.Linq;
+
+namespace Kudu.Services.GitServer.ServiceHookParser
+{
+    public class Github : IServiceHookParser
+    {
+        public bool TryGetRepositoryInfo(HttpRequest request, string bodyDontUse, out RepositoryInfo repositoryInfo)
+        {
+            repositoryInfo = null;
+            string json = request.Form["payload"];
+            JObject payload = JObject.Parse(json);
+
+            var repository = payload.Value<JObject>("repository");
+            if (repository == null)
+            {
+                return false;
+            }
+
+            var info = new RepositoryInfo();
+
+            // github format
+            // { repository: { url: "https//...", private: False }, ref: "", before: "", after: "" } 
+            info.RepositoryUrl = repository.Value<string>("url");
+
+            if (string.IsNullOrEmpty(info.RepositoryUrl))
+            {
+                return false;
+            }
+
+            info.IsPrivate = repository.Value<bool>("private");
+
+            // The format of ref is refs/something/something else
+            // For master it's normally refs/head/master
+            string @ref = payload.Value<string>("ref");
+
+            if (String.IsNullOrEmpty(@ref))
+            {
+                return false;
+            }
+
+            info.Deployer = "GitHub";
+            info.OldRef = payload.Value<string>("before");
+            info.NewRef = payload.Value<string>("after");
+
+            // private repo, use SSH
+            if (info.IsPrivate)
+            {
+                Uri uri = new Uri(info.RepositoryUrl);
+                if (uri.Scheme.StartsWith("http", StringComparison.OrdinalIgnoreCase))
+                {
+                    info.Host = "git@" + uri.Host;
+                    info.RepositoryUrl = info.Host + ":" + uri.AbsolutePath.TrimStart('/');
+                    info.UseSSH = true;
+                }
+            }
+
+            repositoryInfo = info;
+            return true;
+        }
+    }
+}
---
+++ Kudu.Services/GitServer/ServiceHookParser/IServiceHookParser.cs
@@ -0,0 +1,9 @@
+﻿using System.Web;
+
+namespace Kudu.Services.GitServer.ServiceHookParser
+{
+    public interface IServiceHookParser
+    {
+        bool TryGetRepositoryInfo(HttpRequest request, string body, out RepositoryInfo repositoryInfo);
+    }
+}
