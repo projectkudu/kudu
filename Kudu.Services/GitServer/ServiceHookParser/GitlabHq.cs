15914fa425892dcd2b0e2c8252273eebb7c68f45
Remco Ros <r.ros@proscat.nl>
2012-11-27 15:20:39 +0100

    small update to gitlabhq

--- Kudu.Services/GitServer/ServiceHookParser/GitlabHq.cs
+++ Kudu.Services/GitServer/ServiceHookParser/GitlabHq.cs
@@ -12,8 +12,12 @@ namespace Kudu.Services.GitServer.ServiceHookParser
             JObject payload = JObject.Parse(body.Value);
 
             var repository = payload.Value<JObject>("repository");
-            if (repository == null)
+            var userid = payload.Value<int?>("user_id");
+            var username = payload.Value<string>("user_name");
+
+            if (repository == null || userid == null || username == null)
             {
+                // doesn't look like GitlabHQ
                 return false;
             }
 
@@ -23,7 +27,7 @@ namespace Kudu.Services.GitServer.ServiceHookParser
             // { "before":"34d62c0ad9387a8b9274ad77e878e195c342772b", "after":"02652ef69da7ee3d49134a961bffcb50702661ce", "ref":"refs/heads/master", "user_id":1, "user_name":"Remco Ros", "repository":{ "name":"inspectbin", "url":"http://gitlab.proscat.nl/inspectbin", "description":null, "homepage":"http://gitlab.proscat.nl/inspectbin"  }, "commits":[ { "id":"4109312962bb269ecc3a0d7a3c82a119dcd54c8b", "message":"add uservoice", "timestamp":"2012-11-11T14:32:02+01:00", "url":"http://gitlab.proscat.nl/inspectbin/commits/4109312962bb269ecc3a0d7a3c82a119dcd54c8b", "author":{ "name":"Remco Ros", "email":"r.ros@proscat.nl" }}], "total_commits_count":12 }
             info.RepositoryUrl = repository.Value<string>("url");
 
-            // fix for gitlabhq <= v3.1
+            // Currently Gitlab url's are broken.
             if (!info.RepositoryUrl.EndsWith(".git"))
             {
                 info.RepositoryUrl += ".git";
@@ -48,10 +52,10 @@ namespace Kudu.Services.GitServer.ServiceHookParser
             {
                 return false;
             }
-
-            info.Deployer = "GitlabHQ";
             info.OldRef = payload.Value<string>("before");
             info.NewRef = payload.Value<string>("after");
+
+            info.Deployer = "GitlabHQ";
 
             // private repo, use SSH
             if (info.IsPrivate)
