92f4eb651d939feb0ce9c6df4c41edf1ee82f331
Pranav K <prkrishn@hotmail.com>
2012-12-11 14:29:54 -0800

    Minor cleanup in GitDeploymentTests

--- Kudu.FunctionalTests/GitDeploymentTests.cs
+++ Kudu.FunctionalTests/GitDeploymentTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using Kudu.Core.Deployment;
@@ -16,83 +17,77 @@ namespace Kudu.FunctionalTests
         {
             get
             {
-                //// Name, RepoName, RepoUrl, RepoCloneUrl, DefaultBranchName, VerificationText, ExpectedResponseCode, Skip, VerificationLogText
-                yield return new object[] { "Orchard", "Orchard", "https://github.com/KuduApps/Orchard.git", 
-                    "master", "Welcome to Orchard", HttpStatusCode.OK, "" };
+                // RepoCloneUrl, DefaultBranchName, VerificationText, ExpectedResponseCode, VerificationLogText
+                yield return new object[] { "https://github.com/KuduApps/Orchard.git", "master", 
+                    "Welcome to Orchard", HttpStatusCode.OK, "" };
 
-                yield return new object[] { "ProjectWithNoSolution", "ProjectWithNoSolution", "https://github.com/KuduApps/ProjectWithNoSolution.git", 
-                    "master", "Project without solution", HttpStatusCode.OK, "" };
+                yield return new object[] { "https://github.com/KuduApps/ProjectWithNoSolution.git", "master", 
+                    "Project without solution", HttpStatusCode.OK, "" };
 
-                yield return new object[] { "HiddenFoldersAndFiles", "HiddenFoldersAndFiles", "https://github.com/KuduApps/HiddenFoldersAndFiles.git", 
-                    "master", "Hello World", HttpStatusCode.OK, "" };
+                yield return new object[] { "https://github.com/KuduApps/HiddenFoldersAndFiles.git", "master", 
+                    "Hello World", HttpStatusCode.OK, "" };
 
-                yield return new object[] { "WebSiteInSolution", "WebSiteInSolution", "https://github.com/KuduApps/WebSiteInSolution.git", 
-                    "master", "SomeDummyLibrary.Class1", HttpStatusCode.OK, "" };
+                yield return new object[] { "https://github.com/KuduApps/WebSiteInSolution.git", "master", 
+                    "SomeDummyLibrary.Class1", HttpStatusCode.OK, "" };
 
-                yield return new object[] { "kuduglob", "kuduglob", "https://github.com/KuduApps/kuduglob.git", 
-                    "master", "ASP.NET MVC", HttpStatusCode.OK, "酷度" };
+                yield return new object[] { "https://github.com/KuduApps/kuduglob.git", "master", 
+                    "ASP.NET MVC", HttpStatusCode.OK, "酷度" };
 
-                yield return new object[] { "AppWithPostBuildEvent", "AppWithPostBuildEvent", "https://github.com/KuduApps/AppWithPostBuildEvent.git", 
-                    "master", "Hello Kudu", HttpStatusCode.OK, "Deployment successful" };
+                yield return new object[] { "https://github.com/KuduApps/AppWithPostBuildEvent.git", "master", 
+                    "Hello Kudu", HttpStatusCode.OK, "Deployment successful" };
             }
         }
 
         [Theory]
         [PropertyData("AspNetApps")]
-        public void PushAndDeployAspNetApps(string name, string repoName, string repoCloneUrl,
-                                      string defaultBranchName, string verificationText,
+        public void PushAndDeployAspNetApps(string repoCloneUrl, string defaultBranchName, string verificationText,
                                       HttpStatusCode expectedResponseCode, string verificationLogText = null)
         {
-            PushAndDeployApps(name, repoName, repoCloneUrl, defaultBranchName, verificationText, expectedResponseCode, verificationLogText);
+            PushAndDeployApps(repoCloneUrl, defaultBranchName, verificationText, expectedResponseCode, verificationLogText);
         }
-
 
         public static IEnumerable<object[]> NodeApps
         {
             get
             {
-                // Name, RepoName, RepoUrl, RepoCloneUrl, DefaultBranchName, VerificationText, ExpectedResponseCode, Skip, VerificationLogText
-                yield return new object[] { "Express-Template", "Express-Template", "https://github.com/KuduApps/Express-Template.git", 
-                    "master", "Modify this template to jump-start your Node.JS Express Web Pages application", HttpStatusCode.OK, "" };
+                // RepoCloneUrl, DefaultBranchName, VerificationText, ExpectedResponseCode, VerificationLogText
+                yield return new object[] { "https://github.com/KuduApps/Express-Template.git", "master", 
+                    "Modify this template to jump-start your Node.JS Express Web Pages application", HttpStatusCode.OK, "" };
             }
         }
 
         [Theory]
         [PropertyData("NodeApps")]
-        public void PushAndDeployNodeApps(string name, string repoName, string repoCloneUrl,
-                                      string defaultBranchName, string verificationText,
+        public void PushAndDeployNodeApps(string repoCloneUrl, string defaultBranchName, string verificationText,
                                       HttpStatusCode expectedResponseCode, string verificationLogText = null)
         {
             // Ensure node is installed.
             Assert.Contains("nodejs", Environment.GetEnvironmentVariable("Path"), StringComparison.OrdinalIgnoreCase);
-            PushAndDeployApps(name, repoName, repoCloneUrl, defaultBranchName, verificationText, expectedResponseCode, verificationLogText);
+            PushAndDeployApps(repoCloneUrl, defaultBranchName, verificationText, expectedResponseCode, verificationLogText);
         }
 
         public static IEnumerable<object[]> PhpApps
         {
             get
             {
-                // Name, RepoName, RepoUrl, RepoCloneUrl, DefaultBranchName, VerificationText, ExpectedResponseCode, Skip, VerificationLogText
-                yield return new object[] { "PhpBB", "PhpBBweb", "https://github.com/KuduApps/PhpBBweb.git", 
-                    "master", "Welcome to phpBB3", HttpStatusCode.OK, "" };
+                // RepoCloneUrl, DefaultBranchName, VerificationText, ExpectedResponseCode, VerificationLogText
+                yield return new object[] { "https://github.com/KuduApps/PhpBBweb.git", "master", 
+                    "Welcome to phpBB3", HttpStatusCode.OK, "" };
             }
         }
 
         [Theory(Skip="Omitting PHP test since it's hard to set up and doesn't cover anything interesting to Kudu")]
         [PropertyData("PhpApps")]
-        public void PushAndDeployPhpApps(string name, string repoName, string repoCloneUrl,
-                                         string defaultBranchName, string verificationText,
+        public void PushAndDeployPhpApps(string repoCloneUrl, string defaultBranchName, string verificationText,
                                          HttpStatusCode expectedResponseCode, string verificationLogText = null)
         {
-            PushAndDeployApps(name, repoName, repoCloneUrl, defaultBranchName, verificationText, expectedResponseCode, verificationLogText);
+            PushAndDeployApps(repoCloneUrl, defaultBranchName, verificationText, expectedResponseCode, verificationLogText);
         }
 
-        private static void PushAndDeployApps(string name, string repoName, string repoCloneUrl, string defaultBranchName,
+        private static void PushAndDeployApps(string repoCloneUrl, string defaultBranchName,
                                               string verificationText, HttpStatusCode expectedResponseCode, string verificationLogText)
         {
-            string randomTestName = KuduUtils.GetRandomWebsiteName(repoName);
-
-
+            string randomTestName = KuduUtils.GetRandomWebsiteName(Path.GetFileNameWithoutExtension(repoCloneUrl));
             ApplicationManager.Run(randomTestName, appManager =>
             {
                 // Act
