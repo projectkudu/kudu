2453622f69296733763b860cf94cfb64b3052044
davidebbo <david.ebbo@microsoft.com>
2012-12-24 22:13:55 -0800

    Removed use of XUnit [Theory] in functional tests

--- Kudu.FunctionalTests/GitDeploymentTests.cs
+++ Kudu.FunctionalTests/GitDeploymentTests.cs
@@ -13,58 +13,55 @@ namespace Kudu.FunctionalTests
 {
     public class GitDeploymentTests
     {
-        public static IEnumerable<object[]> AspNetApps
+        // ASP.NET apps
+
+        [Fact]
+        public void PushAndDeployAspNetAppOrchard()
         {
-            get
-            {
-                // RepoCloneUrl, DefaultBranchName, VerificationText, ExpectedResponseCode, VerificationLogText
-                yield return new object[] { "https://github.com/KuduApps/Orchard.git", "master", 
-                    "Welcome to Orchard", HttpStatusCode.OK, "" };
-
-                yield return new object[] { "https://github.com/KuduApps/ProjectWithNoSolution.git", "master", 
-                    "Project without solution", HttpStatusCode.OK, "" };
-
-                yield return new object[] { "https://github.com/KuduApps/HiddenFoldersAndFiles.git", "master", 
-                    "Hello World", HttpStatusCode.OK, "" };
-
-                yield return new object[] { "https://github.com/KuduApps/WebSiteInSolution.git", "master", 
-                    "SomeDummyLibrary.Class1", HttpStatusCode.OK, "" };
-
-                yield return new object[] { "https://github.com/KuduApps/kuduglob.git", "master", 
-                    "ASP.NET MVC", HttpStatusCode.OK, "酷度" };
-
-                yield return new object[] { "https://github.com/KuduApps/AppWithPostBuildEvent.git", "master", 
-                    "Hello Kudu", HttpStatusCode.OK, "Deployment successful" };
-            }
+            PushAndDeployApps("https://github.com/KuduApps/Orchard.git", "master", "Welcome to Orchard", HttpStatusCode.OK, "");
         }
 
-        [Theory]
-        [PropertyData("AspNetApps")]
-        public void PushAndDeployAspNetApps(string repoCloneUrl, string defaultBranchName, string verificationText,
-                                      HttpStatusCode expectedResponseCode, string verificationLogText = null)
+        [Fact]
+        public void PushAndDeployAspNetAppProjectWithNoSolution()
         {
-            PushAndDeployApps(repoCloneUrl, defaultBranchName, verificationText, expectedResponseCode, verificationLogText);
+            PushAndDeployApps("https://github.com/KuduApps/ProjectWithNoSolution.git", "master", "Project without solution", HttpStatusCode.OK, "");
         }
 
-        public static IEnumerable<object[]> NodeApps
+        [Fact]
+        public void PushAndDeployAspNetAppHiddenFoldersAndFiles()
         {
-            get
-            {
-                // RepoCloneUrl, DefaultBranchName, VerificationText, ExpectedResponseCode, VerificationLogText
-                yield return new object[] { "https://github.com/KuduApps/Express-Template.git", "master", 
-                    "Modify this template to jump-start your Node.JS Express Web Pages application", HttpStatusCode.OK, "" };
-            }
+            PushAndDeployApps("https://github.com/KuduApps/HiddenFoldersAndFiles.git", "master", "Hello World", HttpStatusCode.OK, "");
         }
 
-        [Theory]
-        [PropertyData("NodeApps")]
-        public void PushAndDeployNodeApps(string repoCloneUrl, string defaultBranchName, string verificationText,
-                                      HttpStatusCode expectedResponseCode, string verificationLogText = null)
+        [Fact]
+        public void PushAndDeployAspNetAppWebSiteInSolution()
         {
-            // Ensure node is installed.
-            Assert.Contains("nodejs", Environment.GetEnvironmentVariable("Path"), StringComparison.OrdinalIgnoreCase);
-            PushAndDeployApps(repoCloneUrl, defaultBranchName, verificationText, expectedResponseCode, verificationLogText);
+            PushAndDeployApps("https://github.com/KuduApps/WebSiteInSolution.git", "master", "SomeDummyLibrary.Class1", HttpStatusCode.OK, "");
         }
+
+        [Fact]
+        public void PushAndDeployAspNetAppKuduGlob()
+        {
+            PushAndDeployApps("https://github.com/KuduApps/kuduglob.git", "master", "ASP.NET MVC", HttpStatusCode.OK, "酷度");
+        }
+
+        [Fact]
+        public void PushAndDeployAspNetAppAppWithPostBuildEvent()
+        {
+            PushAndDeployApps("https://github.com/KuduApps/AppWithPostBuildEvent.git", "master", "Hello Kudu", HttpStatusCode.OK, "Deployment successful");
+        }
+
+
+        // Node apps
+
+        [Fact]
+        public void PushAndDeployNodeAppExpress()
+        {
+            PushAndDeployApps("https://github.com/KuduApps/Express-Template.git", "master", "Modify this template to jump-start your Node.JS Express Web Pages application", HttpStatusCode.OK, "");
+        }
+
+
+        // Other apps
 
         [Fact]
         public void CustomDeploymentScriptShouldHaveDeploymentSetting()
@@ -89,24 +86,6 @@ namespace Kudu.FunctionalTests
                 Assert.Equal(DeployStatus.Success, results[0].Status);
                 KuduAssert.VerifyLogOutput(appManager, results[0].Id, verificationLogText.Trim());
             });
-        }
-
-        public static IEnumerable<object[]> PhpApps
-        {
-            get
-            {
-                // RepoCloneUrl, DefaultBranchName, VerificationText, ExpectedResponseCode, VerificationLogText
-                yield return new object[] { "https://github.com/KuduApps/PhpBBweb.git", "master", 
-                    "Welcome to phpBB3", HttpStatusCode.OK, "" };
-            }
-        }
-
-        [Theory(Skip="Omitting PHP test since it's hard to set up and doesn't cover anything interesting to Kudu")]
-        [PropertyData("PhpApps")]
-        public void PushAndDeployPhpApps(string repoCloneUrl, string defaultBranchName, string verificationText,
-                                         HttpStatusCode expectedResponseCode, string verificationLogText = null)
-        {
-            PushAndDeployApps(repoCloneUrl, defaultBranchName, verificationText, expectedResponseCode, verificationLogText);
         }
 
         private static void PushAndDeployApps(string repoCloneUrl, string defaultBranchName,
