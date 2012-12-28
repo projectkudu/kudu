5f9268f1e2ff6b43acaee38b2553049391e9a088
Amit Apple <amitap@microsoft.com>
2012-12-27 19:14:01 -0800

    Added error handling to updateNodeModules.cmd script.

--- Kudu.Services.Web/updateNodeModules.cmd
+++ Kudu.Services.Web/updateNodeModules.cmd
@@ -16,4 +16,15 @@ if exist %1\node_modules\kudusync\bin\kudusync (
   cmd /c npm install kudusync
 )
 
+IF %ERRORLEVEL% NEQ 0 goto error
+
 popd
+
+goto end
+
+:error
+echo An error has occured during npm install.
+exit /b 1
+
+:end
+echo Finished successfully.
