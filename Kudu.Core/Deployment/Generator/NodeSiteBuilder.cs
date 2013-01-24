c7195d754ba3a89e96da44c5809e885c309c4137
Amit Apple <amitap@microsoft.com>
2013-01-23 17:54:03 -0800

    Clean the web.config file from the repository after the node deployment is complete (removes the file if not part of the repository). This change fixes issue #308.

--- Kudu.Core/Deployment/Generator/NodeSiteBuilder.cs
+++ Kudu.Core/Deployment/Generator/NodeSiteBuilder.cs
@@ -1,7 +1,9 @@
 ﻿using Kudu.Contracts.Settings;
+using Kudu.Core.SourceControl.Git;
 using System;
 using System.Globalization;
 using System.IO.Abstractions;
+using System.Threading.Tasks;
 
 namespace Kudu.Core.Deployment.Generator
 {
@@ -10,6 +12,36 @@ namespace Kudu.Core.Deployment.Generator
         public NodeSiteBuilder(IEnvironment environment, IDeploymentSettingsManager settings, IBuildPropertyProvider propertyProvider, string repositoryPath, string projectPath)
             : base(environment, settings, propertyProvider, repositoryPath, projectPath, "--node")
         {
+        }
+
+        public override Task Build(DeploymentContext context)
+        {
+            try
+            {
+                return base.Build(context);
+            }
+            finally
+            {
+                SafeCleanWebConfig(context);
+            }
+        }
+
+        private void SafeCleanWebConfig(DeploymentContext context)
+        {
+            try
+            {
+                var git = new GitExecutable(Environment.RepositoryPath, DeploymentSettings.GetCommandIdleTimeout());
+                if (!String.IsNullOrEmpty(HomePath))
+                {
+                    git.SetHomePath(HomePath);
+                }
+                var args = String.Format(CultureInfo.InvariantCulture, "clean -f {0}\\web.config", this.ProjectPath);
+                git.Execute(args);
+            }
+            catch (Exception ex)
+            {
+                context.Logger.Log(ex);
+            }
         }
     }
 }
