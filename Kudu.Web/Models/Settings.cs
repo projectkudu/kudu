95cf397c72dcfaeba664ed42fc61984eb18cf80d
Amit Apple <amitap@microsoft.com>
2013-03-05 13:53:04 -0800

    Changed settings to internal.

--- Kudu.Web/Models/Settings.cs
+++ Kudu.Web/Models/Settings.cs
@@ -1,15 +1,18 @@
 ﻿using System.Collections.Specialized;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Kudu.Web.Models
 {
-    public class Settings : ISettings
+    internal class Settings : ISettings
     {
+        [SuppressMessage("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode", Justification = "By design")]
         public NameValueCollection AppSettings
         {
             get;
             set;
         }
 
+        [SuppressMessage("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode", Justification = "By design")]
         public NameValueCollection ConnectionStrings
         {
             get;
