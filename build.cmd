0f21eb80d9cf2bb9df7ca7a5053bf1dd5288d29c
Pranav K <prkrishn@hotmail.com>
2013-03-18 11:36:45 -0700

    * Prevent xml doc warnings in Kudu.Services.csproj in release builds * Change build.cmd to build in release by default to mirror CI * Updated Kudu.targets to always treat warnings as errors

--- build.cmd
+++ build.cmd
@@ -1,6 +1,6 @@
 @echo Off
 set config=%1
 if "%config%" == "" (
-   set config=debug
+   set config=Release
 )
 %WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild Build\Build.proj /p:Configuration="%config%";ExcludeXmlAssemblyFiles=false /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false /m
