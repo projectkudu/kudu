9aabdd96b4184a2afed8172294987aa92b86700a
Amit Apple <amitap@microsoft.com>
2013-03-26 16:41:21 +0200

    Update detection of node.js sites.

--- Kudu.Core/Deployment/Generator/NodeSiteEnabler.cs
+++ Kudu.Core/Deployment/Generator/NodeSiteEnabler.cs
@@ -9,12 +9,12 @@ namespace Kudu.Core.Deployment.Generator
 {
     public static class NodeSiteEnabler
     {
-        private static readonly string[] NodeStartFiles = new[] { "server.js", "app.js" };
+        private static readonly string[] NodeDetectionFiles = new[] { "server.js", "app.js", "package.json" };
 
         public static bool LooksLikeNode(IFileSystem fileSystem, string siteFolder)
         {
             // Check if any of the known start pages exist
-            foreach (var nodeDetectionFile in NodeStartFiles)
+            foreach (var nodeDetectionFile in NodeDetectionFiles)
             {
                 string fullPath = Path.Combine(siteFolder, nodeDetectionFile);
                 if (fileSystem.File.Exists(fullPath))
