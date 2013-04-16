c9bdcd3b6c3ae0a1557d49022091ad473e6a263e
Amit Apple <amitap@microsoft.com>
2013-04-15 21:08:34 -0700

    Update node detection logic, if a known startup file exists in the root directory, treat web site as basic instead of node. Fixes #560

--- Kudu.Core/Deployment/Generator/NodeSiteEnabler.cs
+++ Kudu.Core/Deployment/Generator/NodeSiteEnabler.cs
@@ -9,10 +9,26 @@ namespace Kudu.Core.Deployment.Generator
 {
     public static class NodeSiteEnabler
     {
+        private static readonly string[] IisStartupFiles = new[]
+        {
+            "default.htm", "default.html", "default.asp", "index.htm", "index.html", "iisstart.htm", "default.aspx", "index.php", "hostingstart.html"
+        };
+
         private static readonly string[] NodeDetectionFiles = new[] { "server.js", "app.js", "package.json" };
 
         public static bool LooksLikeNode(IFileSystem fileSystem, string siteFolder)
         {
+            // Check if any of the known iis start pages exist
+            // If so, then it is not a node.js web site
+            foreach (var iisStartupFile in IisStartupFiles)
+            {
+                string fullPath = Path.Combine(siteFolder, iisStartupFile);
+                if (fileSystem.File.Exists(fullPath))
+                {
+                    return false;
+                }
+            }
+
             // Check if any of the known start pages exist
             foreach (var nodeDetectionFile in NodeDetectionFiles)
             {
