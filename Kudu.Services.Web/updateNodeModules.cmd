337ff390fd839d46e8aaff48a094add0f84d76c4
Amit Apple <amitap@microsoft.com>
2013-04-18 18:16:46 -0700

    Remove long directory from azure-cli

--- Kudu.Services.Web/updateNodeModules.cmd
+++ Kudu.Services.Web/updateNodeModules.cmd
@@ -9,13 +9,19 @@ set counter=0
 set /a counter+=1
 echo Attempt %counter% out of %attempts%
 
-if exist %1\node_modules\azure-cli\bin\azure (
+if exist node_modules\azure-cli\bin\azure (
   cmd /c npm update https://github.com/amitapl/azure-sdk-tools-xplat/tarball/kudu_s21_5
 ) else (
   cmd /c npm install https://github.com/amitapl/azure-sdk-tools-xplat/tarball/kudu_s21_5
 )
 
 IF %ERRORLEVEL% NEQ 0 goto error
+
+set long_unrequired_directory=node_modules\azure-cli\node_modules\azure\node_modules\request\node_modules\form-data\node_modules\combined-stream\node_modules\delayed-stream\test
+if exist %long_unrequired_directory% (
+  rmdir /s /q %long_unrequired_directory%
+  IF %ERRORLEVEL% NEQ 0 goto error
+)
 
 goto end
 
