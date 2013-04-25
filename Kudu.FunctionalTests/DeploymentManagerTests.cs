da4afd18d706d2e49dc0752f4c6729f6c2240a82
Amit Apple <amitap@microsoft.com>
2013-04-25 16:19:08 -0700

    Added traces to the vfs tests. Updated PullApiTestConsecutivePushesGetQueued test, made deployment run for 10 seconds.

--- Kudu.FunctionalTests/DeploymentManagerTests.cs
+++ Kudu.FunctionalTests/DeploymentManagerTests.cs
@@ -322,14 +322,14 @@ namespace Kudu.FunctionalTests
             string githubPayload = @"{ ""after"": ""ea1c6d7ea669c816dd5f86206f7b47b228fdcacd"", ""before"": ""7e2a599e2d28665047ec347ab36731c905c95e8b"",  ""commits"": [ { ""added"": [], ""author"": { ""email"": ""prkrishn@hotmail.com"", ""name"": ""Pranav K"", ""username"": ""pranavkm"" }, ""id"": ""43acf30efa8339103e2bed5c6da1379614b00572"", ""message"": ""Changes from master again"", ""modified"": [ ""Hello.txt"" ], ""timestamp"": ""2012-12-17T17:32:15-08:00"" } ], ""compare"": ""https://github.com/KuduApps/GitHookTest/compare/7e2a599e2d28...7e2a599e2d28"", ""created"": false, ""deleted"": false, ""forced"": false, ""head_commit"": { ""added"": [ "".gitignore"", ""SimpleWebApplication.sln"", ""SimpleWebApplication/About.aspx"", ""SimpleWebApplication/About.aspx.cs"", ""SimpleWebApplication/About.aspx.designer.cs"", ""SimpleWebApplication/Account/ChangePassword.aspx"", ""SimpleWebApplication/Account/ChangePassword.aspx.cs"", ""SimpleWebApplication/Account/ChangePassword.aspx.designer.cs"", ""SimpleWebApplication/Account/ChangePasswordSuccess.aspx"", ""SimpleWebApplication/Account/ChangePasswordSuccess.aspx.cs"", ""SimpleWebApplication/Account/ChangePasswordSuccess.aspx.designer.cs"", ""SimpleWebApplication/Account/Login.aspx"", ""SimpleWebApplication/Account/Login.aspx.cs"", ""SimpleWebApplication/Account/Login.aspx.designer.cs"", ""SimpleWebApplication/Account/Register.aspx"", ""SimpleWebApplication/Account/Register.aspx.cs"", ""SimpleWebApplication/Account/Register.aspx.designer.cs"", ""SimpleWebApplication/Account/Web.config"", ""SimpleWebApplication/Default.aspx"", ""SimpleWebApplication/Default.aspx.cs"", ""SimpleWebApplication/Default.aspx.designer.cs"", ""SimpleWebApplication/Global.asax"", ""SimpleWebApplication/Global.asax.cs"", ""SimpleWebApplication/Properties/AssemblyInfo.cs"", ""SimpleWebApplication/Scripts/jquery-1.4.1-vsdoc.js"", ""SimpleWebApplication/Scripts/jquery-1.4.1.js"", ""SimpleWebApplication/Scripts/jquery-1.4.1.min.js"", ""SimpleWebApplication/SimpleWebApplication.csproj"", ""SimpleWebApplication/Site.Master"", ""SimpleWebApplication/Site.Master.cs"", ""SimpleWebApplication/Site.Master.designer.cs"", ""SimpleWebApplication/Styles/Site.css"", ""SimpleWebApplication/Web.Debug.config"", ""SimpleWebApplication/Web.Release.config"", ""SimpleWebApplication/Web.config"" ], ""author"": { ""email"": ""david.ebbo@microsoft.com"", ""name"": ""davidebbo"", ""username"": ""davidebbo"" }, ""committer"": { ""email"": ""david.ebbo@microsoft.com"", ""name"": ""davidebbo"", ""username"": ""davidebbo"" }, ""distinct"": false, ""id"": ""7e2a599e2d28665047ec347ab36731c905c95e8b"", ""message"": ""Initial"", ""modified"": [], ""removed"": [], ""timestamp"": ""2011-11-21T23:07:42-08:00"", ""url"": ""https://github.com/KuduApps/GitHookTest/commit/7e2a599e2d28665047ec347ab36731c905c95e8b"" }, ""pusher"": { ""name"": ""none"" }, ""ref"": ""refs/heads/master"", ""repository"": { ""created_at"": ""2012-06-28T00:07:55-07:00"", ""description"": """", ""fork"": false, ""forks"": 1, ""has_downloads"": true, ""has_issues"": true, ""has_wiki"": true, ""language"": ""ASP"", ""name"": ""GitHookTest"", ""open_issues"": 0, ""organization"": ""KuduApps"", ""owner"": { ""email"": ""kuduapps@hotmail.com"", ""name"": ""KuduApps"" }, ""private"": false, ""pushed_at"": ""2012-06-28T00:11:48-07:00"", ""size"": 188, ""url"": ""https://github.com/KuduApps/SimpleWebApplication"", ""watchers"": 1 } }";
             string appName = "PullApiTestGitHubFormat";
 
-            await ApplicationManager.RunAsync(appName, async appManager => 
+            await ApplicationManager.RunAsync(appName, async appManager =>
             {
                 var post = new Dictionary<string, string>
                 {
                     { "payload", githubPayload }
                 };
 
-                await DeployPayloadHelperAsync(appManager, client => 
+                await DeployPayloadHelperAsync(appManager, client =>
                 {
                     client.DefaultRequestHeaders.Add("X-Github-Event", "push");
                     return client.PostAsync("deploy?scmType=GitHub", new FormUrlEncodedContent(post));
@@ -390,7 +390,7 @@ namespace Kudu.FunctionalTests
                 var client = CreateClient(appManager);
 
                 await appManager.SettingsManager.SetValue("branch", "default");
-                
+
                 client.DefaultRequestHeaders.Add("User-Agent", "Bitbucket.org");
                 var post = new Dictionary<string, string>
                 {
@@ -398,7 +398,6 @@ namespace Kudu.FunctionalTests
                 };
 
                 (await client.PostAsync("deploy?scmType=BitbucketHg", new FormUrlEncodedContent(post))).EnsureSuccessful();
-
 
                 var results = (await appManager.DeploymentManager.GetResultsAsync()).ToList();
                 Assert.Equal(1, results.Count);
@@ -420,7 +419,7 @@ namespace Kudu.FunctionalTests
                 if (!SshHelper.PrepareSSHEnv(appManager.SSHKeyManager))
                 {
                     // Run SSH tests only if the key is present
-                    return; 
+                    return;
                 }
                 var client = CreateClient(appManager);
                 await appManager.SettingsManager.SetValue("branch", "Test-Branch");
@@ -585,7 +584,7 @@ namespace Kudu.FunctionalTests
                 {
                     { "payload", payload }
                 };
-                
+
                 await DeployPayloadHelperAsync(appManager, client => client.PostAsync("deploy", new FormUrlEncodedContent(post)));
 
                 var results = (await appManager.DeploymentManager.GetResultsAsync()).ToList();
@@ -618,6 +617,8 @@ namespace Kudu.FunctionalTests
 
                 Task<HttpResponseMessage> responseTask1 = null;
                 Task<HttpResponseMessage> responseTask2 = null;
+
+                await appManager.SettingsManager.SetValue("COMMAND", "sleep 10 & xcopy /y %DEPLOYMENT_SOURCE%\\index.html %DEPLOYMENT_TARGET%\\");
 
                 // Start the first fetch request for the master branch
                 responseTask1 = PostPayloadHelperAsync(appManager, client => client.PostAsync("deploy", new FormUrlEncodedContent(postMaster)));
