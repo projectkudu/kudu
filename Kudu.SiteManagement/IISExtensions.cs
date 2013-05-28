24e32264b9faa0f38bc67a9e88501a27a1b92417
Pranav <prkrishn@hotmail.com>
2013-05-28 16:31:49 -0700

    Set the ScmType when creating new sites Fixes #632

--- Kudu.SiteManagement/IISExtensions.cs
+++ Kudu.SiteManagement/IISExtensions.cs
@@ -1,45 +1,69 @@
-﻿using IIS = Microsoft.Web.Administration;
+﻿using System;
+using System.Threading.Tasks;
+using IIS = Microsoft.Web.Administration;
 
 namespace Kudu.SiteManagement
 {
     public static class IISExtensions
     {
-        public static void StartAndWait(this IIS.Site site)
-        {
-            var wait = new PollingWait(() => site.Start(), () => site.State == IIS.ObjectState.Started);
+        private static readonly TimeSpan _waitInterval = TimeSpan.FromMilliseconds(500);
+        private static readonly TimeSpan _maxWaitInterval = TimeSpan.FromMinutes(5);
 
-            wait.Invoke();
+        public static async Task WaitForState(this IIS.Site site, IIS.ObjectState state)
+        {
+            await WaitForState(() => site.State, state);
         }
 
-        public static void StopAndWait(this IIS.Site site)
+        public static async Task StopAndWait(this IIS.Site site)
         {
-            var wait = new PollingWait(() => site.Stop(), () => site.State == IIS.ObjectState.Stopped);
-
-            wait.Invoke();
+            site.Stop();
+            await WaitForState(() => site.State, IIS.ObjectState.Stopped);
         }
 
-        public static void StartAndWait(this IIS.ApplicationPool appPool)
+        public static async Task StartAndWait(this IIS.ApplicationPool appPool)
         {
-            var wait = new PollingWait(() => appPool.Start(), () => appPool.State == IIS.ObjectState.Started);
-
-            wait.Invoke();
+            appPool.Start();
+            await WaitForState(() => appPool.State, IIS.ObjectState.Started);
         }
 
-        public static void StopAndWait(this IIS.ApplicationPool appPool)
+        public static async Task StopAndWait(this IIS.ApplicationPool appPool)
         {
-            var wait = new PollingWait(() => appPool.Stop(), () => appPool.State == IIS.ObjectState.Stopped);
-
-            wait.Invoke();
+            appPool.Stop();
+            await appPool.WaitForState(IIS.ObjectState.Stopped);
         }
 
-        public static void WaitForState(this IIS.ApplicationPool appPool, IIS.ObjectState state)
+        public static async Task WaitForState(this IIS.ApplicationPool appPool, IIS.ObjectState state)
         {
-            new PollingWait(() => { }, () => appPool.State == state).Invoke();
+            await WaitForState(() => appPool.State, state);
         }
 
-        public static void WaitForState(this IIS.Site site, IIS.ObjectState state)
+        private static async Task WaitForState(Func<IIS.ObjectState> getState, IIS.ObjectState state)
         {
-            new PollingWait(() => { }, () => site.State == state).Invoke();
+            TimeSpan totalWait = TimeSpan.Zero;
+
+            IIS.ObjectState? currentState;
+            while ((currentState = SafeGetState(getState)) != state)
+            {
+                totalWait += _waitInterval;
+                if (totalWait > _maxWaitInterval)
+                {
+                    throw new InvalidOperationException(String.Format("State unchanged after {0} seconds. Expected state: '{1}', actual state: '{2}'.",
+                        totalWait.TotalSeconds, state, currentState));
+                }
+                await Task.Delay(_waitInterval);
+            }
+        }
+
+        private static IIS.ObjectState? SafeGetState(Func<IIS.ObjectState> getState)
+        {
+            try
+            {
+                return getState();
+            }
+            catch
+            {
+                return null;
+            }
         }
     }
 }
