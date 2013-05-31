8374006d34b5c907bae6fd050e16f6351a168452
Faiz Shaikh <fashaikh@micrsoft.com>
2013-05-30 22:25:59 -0700

    Making class public

--- Kudu.FunctionalTests/LargeRepoTests.cs
+++ Kudu.FunctionalTests/LargeRepoTests.cs
@@ -14,7 +14,7 @@ using TimeoutException = System.TimeoutException;
 
 namespace Kudu.FunctionalTests
 {
-    class LargeRepoTests
+    public class LargeRepoTests
     {
         private static readonly bool shouldRunLargeRepoTests = !String.IsNullOrEmpty(Environment.GetEnvironmentVariable("RUN_LARGE_REPO_TESTS"));
 
