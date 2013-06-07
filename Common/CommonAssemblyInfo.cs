ddd0a6d8993c3c577f02cbc4811afefc8137d121
davidebbo <david.ebbo@microsoft.com>
2013-06-06 23:08:54 -0700

    Change Kudu minor version to 23.

--- Common/CommonAssemblyInfo.cs
+++ Common/CommonAssemblyInfo.cs
@@ -8,8 +8,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 
 // If you change this version, make sure to change Build\build.proj accordingly
-[assembly: AssemblyVersion("1.22.0.0")]
-[assembly: AssemblyFileVersion("1.22.0.0")]
+[assembly: AssemblyVersion("1.23.0.0")]
+[assembly: AssemblyFileVersion("1.23.0.0")]
 
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(false)]
