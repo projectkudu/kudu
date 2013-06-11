WeAreMammoth/kudu-obsolete

Adding post deployment hook capability

Kudu.Core.Test/Deployment/DeploymentManagerFacts.cs
@@ -1,13 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO.Abstractions;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Kudu.Contracts.Infrastructure;
 using Kudu.Contracts.Settings;
 using Kudu.Contracts.Tracing;
 using Kudu.Core.Deployment;
+using Kudu.Core.Hooks;
 using Kudu.Core.SourceControl;
 using Kudu.Core.Tracing;
 using Moq;
@@ -65,7 +62,6 @@ namespace Kudu.Core.Test.Deployment
         {
             public string Id { get; set; }
 
-
             public DeployStatus Status { get; set; }
 
             public string StatusText { get; set; }
@@ -98,17 +94,16 @@ namespace Kudu.Core.Test.Deployment
             }
         }
 
-
-
         private static DeploymentManager CreateDeploymentManager(
-                                 ISiteBuilderFactory builderFactory= null,
-                                 IEnvironment environment= null,
-                                 IFileSystem fileSystem= null,
-                                 ITraceFactory traceFactory= null,
-                                 IDeploymentSettingsManager settings= null,
-                                 IDeploymentStatusManager status= null,
-                                 IOperationLock deploymentLock= null,
-                                 ILogger globalLogger = null)
+                                 ISiteBuilderFactory builderFactory = null,
+                                 IEnvironment environment = null,
+                                 IFileSystem fileSystem = null,
+                                 ITraceFactory traceFactory = null,
+                                 IDeploymentSettingsManager settings = null,
+                                 IDeploymentStatusManager status = null,
+                                 IOperationLock deploymentLock = null,
+                                 ILogger globalLogger = null,
+                                 IHooksManager hooksManager = null)
         {
             builderFactory = builderFactory ?? Mock.Of<ISiteBuilderFactory>();
             environment = environment ?? Mock.Of<IEnvironment>();
@@ -119,7 +114,7 @@ namespace Kudu.Core.Test.Deployment
             deploymentLock = deploymentLock ?? Mock.Of<IOperationLock>();
             globalLogger = globalLogger ?? Mock.Of<ILogger>();
 
-            return new DeploymentManager(builderFactory, environment, fileSystem, traceFactory, settings, status, deploymentLock, globalLogger);
+            return new DeploymentManager(builderFactory, environment, fileSystem, traceFactory, settings, status, deploymentLock, globalLogger, hooksManager);
         }
     }
 }

Kudu.Services.Web/App_Start/NinjectServices.cs
@@ -16,6 +16,7 @@ using Kudu.Contracts.Tracing;
 using Kudu.Core;
 using Kudu.Core.Commands;
 using Kudu.Core.Deployment;
+using Kudu.Core.Hooks;
 using Kudu.Core.Infrastructure;
 using Kudu.Core.Settings;
 using Kudu.Core.SourceControl;
@@ -168,6 +169,9 @@ namespace Kudu.Services.Web.App_Start
             kernel.Bind<ISiteBuilderFactory>().To<SiteBuilderFactoryDispatcher>()
                                              .InRequestScope();
 
+            kernel.Bind<IHooksManager>().To<HooksManager>()
+                                             .InRequestScope();
+
             kernel.Bind<ILogger>().ToMethod(context => GetLogger(environment, context.Kernel))
                                              .InRequestScope();
 
@@ -306,6 +310,10 @@ namespace Kudu.Services.Web.App_Start
             routes.MapHttpRoute("one-process-get", "diagnostics/processes/{id}", new { controller = "Process", action = "GetProcess" }, new { verb = new HttpMethodConstraint("GET") });
             routes.MapHttpRoute("one-process-delete", "diagnostics/processes/{id}", new { controller = "Process", action = "KillProcess" }, new { verb = new HttpMethodConstraint("DELETE") });
             routes.MapHttpRoute("one-process-dump", "diagnostics/processes/{id}/dump", new { controller = "Process", action = "MiniDump" }, new { verb = new HttpMethodConstraint("GET") });
+
+            // Hooks
+            routes.MapHttpRoute("subscribe-hook", "hooks/subscribe", new { controller = "Hooks", action = "Subscribe" }, new { verb = new HttpMethodConstraint("POST") });
+            routes.MapHttpRoute("unsubscribe-hook", "hooks/unsubscribe", new { controller = "Hooks", action = "Unsubscribe" }, new { verb = new HttpMethodConstraint("POST") });
         }
 
         private static ITracer GetTracer(IEnvironment environment, IKernel kernel)

projectkudu/kudu

Kudu.Core.Test/Deployment/DeploymentManagerFacts.cs
@@ -1,13 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO.Abstractions;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Kudu.Contracts.Infrastructure;
 using Kudu.Contracts.Settings;
 using Kudu.Contracts.Tracing;
 using Kudu.Core.Deployment;
+using Kudu.Core.Hooks;
 using Kudu.Core.SourceControl;
 using Kudu.Core.Tracing;
 using Moq;
@@ -65,7 +62,6 @@ namespace Kudu.Core.Test.Deployment
         {
             public string Id { get; set; }
 
-
             public DeployStatus Status { get; set; }
 
             public string StatusText { get; set; }
@@ -100,17 +96,16 @@ namespace Kudu.Core.Test.Deployment
             }
         }
 
-
-
         private static DeploymentManager CreateDeploymentManager(
-                                 ISiteBuilderFactory builderFactory= null,
-                                 IEnvironment environment= null,
-                                 IFileSystem fileSystem= null,
-                                 ITraceFactory traceFactory= null,
-                                 IDeploymentSettingsManager settings= null,
-                                 IDeploymentStatusManager status= null,
-                                 IOperationLock deploymentLock= null,
-                                 ILogger globalLogger = null)
+                                 ISiteBuilderFactory builderFactory = null,
+                                 IEnvironment environment = null,
+                                 IFileSystem fileSystem = null,
+                                 ITraceFactory traceFactory = null,
+                                 IDeploymentSettingsManager settings = null,
+                                 IDeploymentStatusManager status = null,
+                                 IOperationLock deploymentLock = null,
+                                 ILogger globalLogger = null,
+                                 IWebHooksManager hooksManager = null)
         {
             builderFactory = builderFactory ?? Mock.Of<ISiteBuilderFactory>();
             environment = environment ?? Mock.Of<IEnvironment>();
@@ -121,7 +116,7 @@ namespace Kudu.Core.Test.Deployment
             deploymentLock = deploymentLock ?? Mock.Of<IOperationLock>();
             globalLogger = globalLogger ?? Mock.Of<ILogger>();
 
-            return new DeploymentManager(builderFactory, environment, fileSystem, traceFactory, settings, status, deploymentLock, globalLogger);
+            return new DeploymentManager(builderFactory, environment, fileSystem, traceFactory, settings, status, deploymentLock, globalLogger, hooksManager);
         }
     }
 }

Kudu.Services.Web/App_Start/NinjectServices.cs
@@ -17,6 +17,7 @@ using Kudu.Core;
 using Kudu.Core.Commands;
 using Kudu.Core.Deployment;
 using Kudu.Core.Deployment.Generator;
+using Kudu.Core.Hooks;
 using Kudu.Core.Infrastructure;
 using Kudu.Core.Settings;
 using Kudu.Core.SourceControl;
@@ -128,14 +129,17 @@ namespace Kudu.Services.Web.App_Start
             string deploymentLockPath = Path.Combine(lockPath, Constants.DeploymentLockFile);
             string statusLockPath = Path.Combine(lockPath, Constants.StatusLockFile);
             string sshKeyLockPath = Path.Combine(lockPath, Constants.SSHKeyLockFile);
+            string hooksLockPath = Path.Combine(lockPath, Constants.HooksLockFile);
 
             var fileSystem = new FileSystem();
             var deploymentLock = new LockFile(deploymentLockPath, kernel.Get<ITraceFactory>(), fileSystem);
             var statusLock = new LockFile(statusLockPath, kernel.Get<ITraceFactory>(), fileSystem);
             var sshKeyLock = new LockFile(sshKeyLockPath, kernel.Get<ITraceFactory>(), fileSystem);
+            var hooksLock = new LockFile(hooksLockPath, kernel.Get<ITraceFactory>(), fileSystem);
 
             kernel.Bind<IOperationLock>().ToConstant(sshKeyLock).WhenInjectedInto<SSHKeyController>();
             kernel.Bind<IOperationLock>().ToConstant(statusLock).WhenInjectedInto<DeploymentStatusManager>();
+            kernel.Bind<IOperationLock>().ToConstant(hooksLock).WhenInjectedInto<WebHooksManager>();
             kernel.Bind<IOperationLock>().ToConstant(deploymentLock);
 
             var shutdownDetector = new ShutdownDetector();
@@ -168,6 +172,9 @@ namespace Kudu.Services.Web.App_Start
                                              .InRequestScope();
 
             kernel.Bind<ISiteBuilderFactory>().To<SiteBuilderFactory>()
+                                             .InRequestScope();
+
+            kernel.Bind<IWebHooksManager>().To<WebHooksManager>()
                                              .InRequestScope();
 
             kernel.Bind<ILogger>().ToMethod(context => GetLogger(environment, context.Kernel))
@@ -308,6 +315,11 @@ namespace Kudu.Services.Web.App_Start
             routes.MapHttpRoute("one-process-get", "diagnostics/processes/{id}", new { controller = "Process", action = "GetProcess" }, new { verb = new HttpMethodConstraint("GET") });
             routes.MapHttpRoute("one-process-delete", "diagnostics/processes/{id}", new { controller = "Process", action = "KillProcess" }, new { verb = new HttpMethodConstraint("DELETE") });
             routes.MapHttpRoute("one-process-dump", "diagnostics/processes/{id}/dump", new { controller = "Process", action = "MiniDump" }, new { verb = new HttpMethodConstraint("GET") });
+
+            // Hooks
+            routes.MapHttpRoute("subscribe-hook", "hooks/subscribe", new { controller = "WebHooks", action = "Subscribe" }, new { verb = new HttpMethodConstraint("POST") });
+            routes.MapHttpRoute("unsubscribe-hook", "hooks/unsubscribe", new { controller = "WebHooks", action = "Unsubscribe" }, new { verb = new HttpMethodConstraint("DELETE") });
+            routes.MapHttpRoute("get-hooks", "hooks", new { controller = "WebHooks", action = "GetWebHooks" }, new { verb = new HttpMethodConstraint("GET") });
         }
 
         private static ITracer GetTracer(IEnvironment environment, IKernel kernel)