14157e84976fa1226c494e1c68af04d439d497ff
Amit Apple <amitap@microsoft.com>
2013-06-11 12:24:58 -0700

    Allow Verbose output from Deployment Script Fixes #624

--- Kudu.Services.Web/updateNodeModules.cmd
+++ Kudu.Services.Web/updateNodeModules.cmd
@@ -10,7 +10,7 @@ set counter=0
 set /a counter+=1
 echo Attempt %counter% out of %attempts%
 
-cmd /c npm install https://github.com/projectkudu/KuduScript/tarball/99816ade95addbfa6cfae7d74b7b616e2eebf772
+cmd /c npm install https://github.com/projectkudu/KuduScript/tarball/3bf82c59679bd6539651cf5751246f34e6fd6a56
 IF %ERRORLEVEL% NEQ 0 goto error
 
 goto end
