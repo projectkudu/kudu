cb55e1ce5f37fe5fc9944e98bbad1f584a715667
Amit Apple <amitap@microsoft.com>
2013-07-09 12:15:50 -0700

    Do not try running post deployment actions if no scripts exist. Fixes #683.

--- Kudu.Core/Scripts/postdeployment.cmd
+++ Kudu.Core/Scripts/postdeployment.cmd
@@ -2,12 +2,14 @@
 
 setlocal enabledelayedexpansion
 
-FOR /F %%A IN ('dir /s /b %POST_DEPLOYMENT_ACTIONS_DIR%\*.cmd %POST_DEPLOYMENT_ACTIONS_DIR%\*.bat') DO (
-  set CURRENT_SCRIPT_FILE=%%A
-  echo Executing - '!CURRENT_SCRIPT_FILE!'
-  call !CURRENT_SCRIPT_FILE!
-  IF !ERRORLEVEL! NEQ 0 goto error
-  echo '!CURRENT_SCRIPT_FILE!' executed successfully
+IF EXIST %POST_DEPLOYMENT_ACTIONS_DIR% (
+  FOR /F %%A IN ('dir /s /b %POST_DEPLOYMENT_ACTIONS_DIR%\*.cmd %POST_DEPLOYMENT_ACTIONS_DIR%\*.bat') DO (
+    set CURRENT_SCRIPT_FILE=%%A
+    echo Executing - '!CURRENT_SCRIPT_FILE!'
+    call !CURRENT_SCRIPT_FILE!
+    IF !ERRORLEVEL! NEQ 0 goto error
+    echo '!CURRENT_SCRIPT_FILE!' executed successfully
+  )
 )
 
 goto end
