17d9d0ced40a6a63409ad4e622d2858ab39e0e20
Pranav <prkrishn@hotmail.com>
2013-07-15 23:18:02 -0700

    * Change variable to WEBSITE_NODE_DEFAULT_VERSION

--- Kudu.Core/Scripts/selectNodeVersion.js
+++ Kudu.Core/Scripts/selectNodeVersion.js
@@ -152,8 +152,8 @@ if (!existsSync(packageJson)) {
 var json = JSON.parse(fs.readFileSync(packageJson, 'utf8'));
 if (typeof json !== 'object' || typeof json.engines !== 'object' || typeof json.engines.node !== 'string') {
     // Attempt to read the pinned node version or fallback to the version of the executing node.exe.
-    if (process.env['WEBSITE_NODE_INITIAL_VERSION']) {
-        var nodeVersion = process.env['WEBSITE_NODE_INITIAL_VERSION'],
+    if (process.env['WEBSITE_NODE_DEFAULT_VERSION']) {
+        var nodeVersion = process.env['WEBSITE_NODE_DEFAULT_VERSION'],
 	        nodeVersionPath = path.resolve(nodejsDir, nodeVersion),
             nodeExePath = path.resolve(nodeVersionPath, 'node.exe'),
             npmPath = getDefaultNpmPath(npmDir, nodeVersionPath);
