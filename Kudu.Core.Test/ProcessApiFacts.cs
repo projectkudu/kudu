a215ffc615f59866128dcab600c351759cfb7fb3
Suwath Ch <suwatch@microsoft.com>
2013-07-20 15:38:33 -0700

    move temp minidump file to LogFiles location

--- Kudu.Core.Test/ProcessApiFacts.cs
+++ Kudu.Core.Test/ProcessApiFacts.cs
@@ -1,8 +1,13 @@
 ﻿using System;
 using System.IO;
 using System.IO.Abstractions;
+using System.Net;
+using System.Net.Http;
+using Kudu.Contracts.Settings;
+using Kudu.Contracts.Tracing;
 using Kudu.Services.Performance;
 using Moq;
+using Newtonsoft.Json.Linq;
 using Xunit;
 
 namespace Kudu.Core.Test
@@ -56,6 +61,29 @@ namespace Kudu.Core.Test
 
             // Assert
             file.Verify(f => f.Delete(path), Times.Once());
+        }
+
+        [Fact]
+        public void MiniDumpFreeModeTests()
+        {
+            var settings = new Mock<IDeploymentSettingsManager>();
+            var controller = new Mock<ProcessController>(Mock.Of<ITracer>(), null, settings.Object, null);
+
+            // Setup
+            controller.Object.Request = new HttpRequestMessage();
+            settings.Setup(s => s.GetValue(SettingsKeys.WebSiteComputeMode, It.IsAny<bool>()))
+                    .Returns("Shared");
+            settings.Setup(s => s.GetValue(SettingsKeys.WebSiteSiteMode, It.IsAny<bool>()))
+                    .Returns("Limited");
+
+            // Test
+            var response = controller.Object.MiniDump(0, 2);
+
+            // Assert
+            Assert.Equal(HttpStatusCode.InternalServerError, response.StatusCode);
+
+            var error = response.Content.ReadAsAsync<JObject>().Result;
+            Assert.Equal("Site mode (Shared|Limited) does not support full minidump.", error.Value<string>("Message"));
         }
     }
 }
