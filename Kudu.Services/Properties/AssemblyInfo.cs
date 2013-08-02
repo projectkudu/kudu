142b92539f0dc22459a890ab49b09d474127e434
Pranav <prkrishn@hotmail.com>
2013-08-01 19:05:00 -0700

    DropboxHelper sync swallows failure during sync Modified it to throw when sync fails.
    Fixes #724

--- Kudu.Services/Properties/AssemblyInfo.cs
+++ Kudu.Services/Properties/AssemblyInfo.cs
@@ -1,4 +1,6 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 
 [assembly: AssemblyTitle("Kudu.Services")]
 [assembly: AssemblyDescription("Services for Kudu")]
+[assembly: InternalsVisibleTo("Kudu.Services.Test")]
