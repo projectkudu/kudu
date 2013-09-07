ae5d3e0ff2975a53708455f57666f5f150a66377
Suwat Ch <suwatch@microsoft.com>
2013-09-07 14:59:15 -0700

    add deploy private kudu script

---
+++ Setup/DeployPrivateKudu.cmd
@@ -0,0 +1,43 @@
+@if "%_echo%" neq "1" @echo off
+
+setlocal
+set _SCRIPT=%~nx0
+set _KUDUZIP=%~dp0Kudu.zip
+set _SCMURI=%~1
+set _CURLEXE=%ProgramFiles(x86)%\git\bin\curl.exe
+
+REM first parameter is the deploy uri with embedded cred
+if "%_SCMURI%" equ "" (
+  call :USAGE 
+  goto :EOF
+)
+
+REM remove any after .net
+set _SCMURI=%_SCMURI:.net=&rem.%
+set _SCMURI=%_SCMURI%.net
+
+if NOT EXIST "%_KUDUZIP%" (
+  @echo "%_KUDUZIP%" does not exists! 
+  goto :EOF
+)
+
+if NOT EXIST "%_CURLEXE%" (
+  @echo "%_CURLEXE%" does not exists! 
+  goto :EOF
+)
+
+@echo.
+@echo "%_CURLEXE%" -k -v -T "%_KUDUZIP%" "%_SCMURI%/zip"
+@echo.
+@call "%_CURLEXE%" -k -v -T "%_KUDUZIP%" "%_SCMURI%/zip"
+@echo.
+@echo Do set Site's AppSetting USE_PRIVATE_EXTENSIONS = 1
+@echo.
+
+exit /b 0
+
+:USAGE
+@echo usage: %_SCRIPT% "<scm_uri>"
+exit /b 0
+ 
+REM testing
