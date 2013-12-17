57b80eb612f0bef20d0a7d1905666a97b3a5b31d
buchizo <buchizo@live.jp>
2013-12-18 03:50:58 +0900

    tweak namespace

--- Kudu.Client/Command/RemoteCommandExecutor.cs
+++ Kudu.Client/Command/RemoteCommandExecutor.cs
@@ -6,7 +6,7 @@ using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 
-namespace Kudu.Client.Deployment
+namespace Kudu.Client.Command
 {
     public class RemoteCommandExecutor : KuduRemoteClientBase
     {
