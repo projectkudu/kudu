f7de1c3cb98f9012a71be106ab7adb23dae8fc23
amitapl <amitapl@outlook.com>
2014-01-22 18:47:11 -0800

    Added powershell script as a runnable script for webjobs Fixes #985

---
+++ Kudu.Core/Jobs/PowerShellScriptHost.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace Kudu.Core.Jobs
+{
+    public class PowerShellScriptHost : ScriptHostBase
+    {
+        private static readonly string[] Supported = { ".ps1" };
+
+        public PowerShellScriptHost()
+            : base("PowerShell.exe", "-ExecutionPolicy RemoteSigned -File {0}")
+        {
+        }
+
+        public override IEnumerable<string> SupportedExtensions
+        {
+            get { return Supported; }
+        }
+    }
+}
