95db1362953e751be2e24138bfce00ca8d622675
Mingwei Tian <mingwei.tian@microsoft.com>
2014-06-04 17:12:07 -0700

    Run install.cmd/uninstall.cmd before and after install/uninstall.

--- Kudu.Core/SiteExtensions/SiteExtensionManager.cs
+++ Kudu.Core/SiteExtensions/SiteExtensionManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Globalization;
 using System.IO;
+using System.IO.Abstractions;
 using System.Linq;
 using System.Web;
 using System.Xml;
