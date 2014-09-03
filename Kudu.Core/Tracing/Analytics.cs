f8597250aa44dbf8d983bd18b5fd679382602a1c
Amit Apple <amitap@microsoft.com>
2014-09-03 16:06:11 -0700

    Event source fails when using null parameters. Also reduced number of events for deprecated APIs when they were already sent.

--- Kudu.Core/Tracing/Analytics.cs
+++ Kudu.Core/Tracing/Analytics.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Text;
 using Kudu.Contracts.Settings;
 using Kudu.Core.Infrastructure;
@@ -7,6 +8,8 @@ namespace Kudu.Core.Tracing
 {
     public class Analytics : IAnalytics
     {
+        private static readonly HashSet<string> _deprecatedApiPaths = new HashSet<string>();
+
         private readonly IDeploymentSettingsManager _settings;
         private readonly IServerConfiguration _serverConfiguration;
 
@@ -18,12 +21,25 @@ namespace Kudu.Core.Tracing
 
         public void ProjectDeployed(string projectType, string result, string error, long deploymentDurationInMilliseconds, string siteMode)
         {
-            KuduEventSource.Log.ProjectDeployed(_serverConfiguration.ApplicationName, projectType, result, error, deploymentDurationInMilliseconds, siteMode, _settings.GetValue(SettingsKeys.ScmType));
+            KuduEventSource.Log.ProjectDeployed(
+                _serverConfiguration.ApplicationName,
+                NullToEmptyString(projectType),
+                NullToEmptyString(result),
+                NullToEmptyString(error),
+                deploymentDurationInMilliseconds,
+                NullToEmptyString(siteMode),
+                NullToEmptyString(_settings.GetValue(SettingsKeys.ScmType)));
         }
 
         public void JobStarted(string jobName, string scriptExtension, string jobType, string siteMode, string error)
         {
-            KuduEventSource.Log.WebJobStarted(_serverConfiguration.ApplicationName, jobName, scriptExtension, jobType, siteMode, error);
+            KuduEventSource.Log.WebJobStarted(
+                _serverConfiguration.ApplicationName,
+                NullToEmptyString(jobName),
+                NullToEmptyString(scriptExtension),
+                NullToEmptyString(jobType),
+                NullToEmptyString(siteMode),
+                NullToEmptyString(error));
         }
 
         public void UnexpectedException(Exception exception)
@@ -45,7 +61,27 @@ namespace Kudu.Core.Tracing
 
         public void DeprecatedApiUsed(string route, string userAgent, string method, string path)
         {
-            KuduEventSource.Log.DeprecatedApiUsed(_serverConfiguration.ApplicationName, route, userAgent, method, path);
+            path = NullToEmptyString(path);
+
+            // Try not to send the same event (on the same path) more than once.
+            if (_deprecatedApiPaths.Contains(path))
+            {
+                return;
+            }
+
+            KuduEventSource.Log.DeprecatedApiUsed(
+                _serverConfiguration.ApplicationName,
+                NullToEmptyString(route),
+                NullToEmptyString(userAgent),
+                NullToEmptyString(method),
+                path);
+
+            _deprecatedApiPaths.Add(path);
+        }
+
+        private static string NullToEmptyString(string s)
+        {
+            return s ?? String.Empty;
         }
     }
 }
