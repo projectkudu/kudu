13ef7def6af879f6dedd93996f335d4c65583211
huguesv <hugues_valois@hotmail.com>
2014-10-27 13:48:20 -0700

    Add initial support for Python, based on presence of requirements.txt

---
+++ Kudu.Core/Deployment/Generator/PythonSiteEnabler.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using System.IO.Abstractions;
+using Kudu.Core.Infrastructure;
+
+namespace Kudu.Core.Deployment.Generator
+{
+    public static class PythonSiteEnabler
+    {
+        private static readonly string[] PythonDetectionFiles = new[] { "requirements.txt" };
+
+        public static bool LooksLikePython(string siteFolder)
+        {
+            // If any of the files in PythonDetectionFiles exist
+            // We assume it's python
+            foreach (var pythonDetectionFile in PythonDetectionFiles)
+            {
+                string fullPath = Path.Combine(siteFolder, pythonDetectionFile);
+                if (FileSystemHelpers.FileExists(fullPath))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
