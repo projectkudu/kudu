7d987296da485d4c0d570fa657061f529a0b99f6
huguesv <hugues_valois@hotmail.com>
2014-10-30 17:58:13 -0700

    Avoid false positives by checking for .py files in addition to the check for requirements.txt.  Allow user to opt in to python detection by also looking at runtime.txt, which is used to specify the desired version of Python.

--- Kudu.Core.Test/Deployment/PythonSiteEnablerTests.cs
+++ Kudu.Core.Test/Deployment/PythonSiteEnablerTests.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.IO;
 using System.IO.Abstractions;
+using System.Linq;
+using System.Text;
 using Kudu.Core.Infrastructure;
 using Moq;
 using Xunit;
@@ -10,28 +13,40 @@ namespace Kudu.Core.Deployment.Generator.Test
     public class PythonSiteEnablerTests
     {
         [Theory]
-        [InlineData(true, new string[] { "requirements.txt" })]
-        [InlineData(true, new string[] { "requirements.txt", "default.asp" })]
-        [InlineData(true, new string[] { "requirements.txt", "site.aspx" })]
-        [InlineData(false, new string[0])]
-        [InlineData(false, new string[] { "index.php" })]
-        [InlineData(false, new string[] { "site.aspx" })]
-        [InlineData(false, new string[] { "site.aspx", "index2.aspx" })]
-        [InlineData(false, new string[] { "server.js" })]
-        [InlineData(false, new string[] { "app.js" })]
-        [InlineData(false, new string[] { "package.json" })]
-        public void TestLooksLikePython(bool looksLikePythonExpectedResult, string[] existingFiles)
+        [InlineData(false, new string[] { "requirements.txt" }, null)]
+        [InlineData(true, new string[] { "requirements.txt", "app.py" }, null)]
+        [InlineData(true, new string[] { "requirements.txt", "runtime.txt" }, "python-3.4.1")]
+        [InlineData(false, new string[] { "requirements.txt", "runtime.txt" }, "i run all the time")]
+        [InlineData(false, new string[] { "requirements.txt", "default.asp" }, null)]
+        [InlineData(false, new string[] { "requirements.txt", "site.aspx" }, null)]
+        [InlineData(false, new string[0], null)]
+        [InlineData(false, new string[] { "index.php" }, null)]
+        [InlineData(false, new string[] { "site.aspx" }, null)]
+        [InlineData(false, new string[] { "site.aspx", "index2.aspx" }, null)]
+        [InlineData(false, new string[] { "server.js" }, null)]
+        [InlineData(false, new string[] { "app.js" }, null)]
+        [InlineData(false, new string[] { "package.json" }, null)]
+        public void TestLooksLikePython(bool looksLikePythonExpectedResult, string[] existingFiles, string runtimeTxtBytes)
         {
             Console.WriteLine("Testing: {0}", String.Join(", ", existingFiles));
 
+            var directoryMock = new Mock<DirectoryBase>();
             var fileMock = new Mock<FileBase>();
             var fileSystemMock = new Mock<IFileSystem>();
+
+            directoryMock.Setup(d => d.GetFiles("site", "*.py")).Returns(existingFiles.Where(f => f.EndsWith(".py")).ToArray());
 
             foreach (var existingFile in existingFiles)
             {
                 fileMock.Setup(f => f.Exists("site\\" + existingFile)).Returns(true);
             }
 
+            if (runtimeTxtBytes != null)
+            {
+                fileMock.Setup(f => f.Open("site\\runtime.txt", FileMode.Open, FileAccess.Read, FileShare.ReadWrite | FileShare.Delete)).Returns(new System.IO.MemoryStream(Encoding.ASCII.GetBytes(runtimeTxtBytes)));
+            }
+
+            fileSystemMock.Setup(f => f.Directory).Returns(directoryMock.Object);
             fileSystemMock.Setup(f => f.File).Returns(fileMock.Object);
             FileSystemHelpers.Instance = fileSystemMock.Object;
 
--- Kudu.Core/Deployment/Generator/PythonSiteEnabler.cs
+++ Kudu.Core/Deployment/Generator/PythonSiteEnabler.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.IO.Abstractions;
 using Kudu.Core.Infrastructure;
 
@@ -6,18 +7,38 @@ namespace Kudu.Core.Deployment.Generator
 {
     public static class PythonSiteEnabler
     {
-        private static readonly string[] PythonDetectionFiles = new[] { "requirements.txt" };
+        private const string RequirementsFileName = "requirements.txt";
+        private const string RuntimeFileName = "runtime.txt";
+        private const string PythonFileExtension = "*.py";
 
         public static bool LooksLikePython(string siteFolder)
         {
-            // If any of the files in PythonDetectionFiles exist
-            // We assume it's python
-            foreach (var pythonDetectionFile in PythonDetectionFiles)
+            var reqsFilePath = Path.Combine(siteFolder, RequirementsFileName);
+            if (!FileSystemHelpers.FileExists(reqsFilePath))
             {
-                string fullPath = Path.Combine(siteFolder, pythonDetectionFile);
-                if (FileSystemHelpers.FileExists(fullPath))
+                return false;
+            }
+
+            var pythonFiles = FileSystemHelpers.GetFiles(siteFolder, PythonFileExtension);
+            if (pythonFiles.Length > 0)
+            {
+                return true;
+            }
+
+            // Most Python sites will have at least a .py file in the root, but
+            // some may not. In that case, let them opt in with the runtime.txt
+            // file, which is used to specify the version of Python.
+            var runtimeFilePath = Path.Combine(siteFolder, RuntimeFileName);
+            if (FileSystemHelpers.FileExists(runtimeFilePath))
+            {
+                try
                 {
-                    return true;
+                    var text = FileSystemHelpers.ReadAllTextFromFile(runtimeFilePath);
+                    return text.IndexOf("python", StringComparison.OrdinalIgnoreCase) >= 0;
+                }
+                catch (IOException)
+                {
+                    return false;
                 }
             }
 
