034e26c2c62d6bfdc9ef96b42b734c84c62e9316
nulltoken <emeric.fermas@gmail.com>
2014-11-06 18:57:15 -0800

    Make build.cmd independent of current working directory

--- build.cmd
+++ build.cmd
@@ -1,17 +1,10 @@
 @echo Off
+
+SET BASEDIR=%~dp0
+
 set config=%1
 if "%config%" == "" (
     set config=Release
 )
 
-:: use MSBuild from .net framework by default
-set MsBuildExe="%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild"
-
-:: if there is MSBuild from vs2013, use MSBuild from vs2013 instead
-if exist "%PROGRAMFILES%\MSBuild\12.0\Bin\MsBuild.exe" (
-    set MsBuildExe="%PROGRAMFILES%\MSBuild\12.0\Bin\MsBuild.exe"
-) else if exist "%PROGRAMFILES(X86)%\MSBuild\12.0\Bin\MsBuild.exe" (
-    set MsBuildExe="%PROGRAMFILES(X86)%\MSBuild\12.0\Bin\MsBuild.exe"
-)
-
-%MsBuildExe% Build\Build.proj /p:Configuration="%config%";ExcludeXmlAssemblyFiles=false /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false /m
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild "%BASEDIR%Build\Build.proj" /p:Configuration="%config%";ExcludeXmlAssemblyFiles=false /v:M /fl /flp:LogFile="%BASEDIR%msbuild.log";Verbosity=Normal /nr:false /m
