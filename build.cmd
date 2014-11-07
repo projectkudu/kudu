615b59e7ca34e98cdeaf3b00fa7886da9d38537d
nulltoken <emeric.fermas@gmail.com>
2014-11-06 18:57:15 -0800

    Make build.cmd independent of current working directory

--- build.cmd
+++ build.cmd
@@ -1,6 +1,9 @@
 @echo Off
+
+SET BASEDIR=%~dp0
+
 set config=%1
 if "%config%" == "" (
    set config=Release
 )
-%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild Build\Build.proj /p:Configuration="%config%";ExcludeXmlAssemblyFiles=false /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false /m
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild "%BASEDIR%Build\Build.proj" /p:Configuration="%config%";ExcludeXmlAssemblyFiles=false /v:M /fl /flp:LogFile="%BASEDIR%msbuild.log";Verbosity=Normal /nr:false /m
