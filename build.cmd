4634e9ea3dc91ed93e54828df00d1919deef5cf0
nulltoken <emeric.fermas@gmail.com>
2014-11-08 13:19:07 +0000

    Make build.[cmd|proj] cope with VS2013 msbuild path changes

--- build.cmd
+++ build.cmd
@@ -6,4 +6,13 @@ set config=%1
 if "%config%" == "" (
    set config=Release
 )
-%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild "%BASEDIR%Build\Build.proj" /p:Configuration="%config%";ExcludeXmlAssemblyFiles=false /v:M /fl /flp:LogFile="%BASEDIR%msbuild.log";Verbosity=Normal /nr:false /m
+
+if exist "%ProgramFiles%\MSBuild\12.0\Bin\msbuild.exe" (
+  set MsBuildPath="%ProgramFiles%\MSBuild\12.0\Bin\msbuild.exe"
+) else if defined ProgramFiles(x86) if exist "%ProgramFiles(x86)%\MSBuild\12.0\Bin\msbuild.exe" (
+  set MsBuildPath="%ProgramFiles(x86)%\MSBuild\12.0\Bin\msbuild.exe"
+) else (
+  set MsBuildPath="%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe"
+)
+
+%MsBuildPath% "%BASEDIR%Build\Build.proj" /p:Configuration="%config%";ExcludeXmlAssemblyFiles=false /v:M /fl /flp:LogFile="%BASEDIR%msbuild.log";Verbosity=Normal /nr:false /m
