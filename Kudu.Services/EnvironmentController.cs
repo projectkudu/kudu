087b5af8fe484beb179ae3dc2b22d91930d4c43a
Suwath Ch <suwatch@microsoft.com>
2015-02-11 11:59:52 -0800

    add last site config modified time to environment api

--- Kudu.Services/EnvironmentController.cs
+++ Kudu.Services/EnvironmentController.cs
@@ -1,6 +1,8 @@
-﻿using System.Net;
+﻿using System.IO;
+using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using Kudu.Core;
 using Newtonsoft.Json.Linq;
 
 namespace Kudu.Services
@@ -18,9 +20,20 @@ namespace Kudu.Services
         {
             // Return the version and other api information (in the end)
             // { 
-            //   "version" : "1.0.0"
+            //   "version" : "1.0.0",
+            //   "siteLastModified" : "2015-02-11T18:27:22.7270000Z",
             // }
             var obj = new JObject(new JProperty("version", _version));
+
+            // this file is written by dwas to communicate the last site configuration modified time
+            var lastModifiedFile = Path.Combine(
+                Path.GetDirectoryName(System.Environment.GetEnvironmentVariable("TMP")), 
+                @"config\SiteLastModifiedTime.txt");
+            if (File.Exists(lastModifiedFile))
+            {
+                obj["siteLastModified"] = File.ReadAllText(lastModifiedFile);
+            }
+
             return Request.CreateResponse(HttpStatusCode.OK, obj);
         }
     }
