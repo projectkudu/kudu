817b18fa25ad48b1e36dcb8cfe1675c975846f93
snobu <foo@snobu.org>
2015-08-07 06:47:08 +0000

    Multiple fixes and performance improvements in Ace
    Kudos to Alex Badescu for the h-scrollbar fix (github.com/badescuga)

--- Kudu.Services.Web/Content/Scripts/ace-init.js
+++ Kudu.Services.Web/Content/Scripts/ace-init.js
@@ -1,3 +1,37 @@
+// Bindings borrowed from https://github.com/probonogeek/knockout-ace,
+// initial version (knockout-ace.js) by Ryan Niemeyer.
+// Updated by Scott Messinger, Frederik Raabye, Thomas Hallock, Drew Freyling, and Shane Carr.
+// Custom integration into Kudu by Adrian Calinescu (https://github.com/snobu)
+
+// Act I - The Binding Dance
+ko.bindingHandlers.ace = {
+    init: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
+        var value = ko.utils.unwrapObservable(valueAccessor());
+        editor.setValue(value);
+        editor.gotoLine( 0 );
+        editor.getSession().on("change",function(delta){
+            if (ko.isWriteableObservable(valueAccessor())) {
+                valueAccessor()( editor.getValue() );
+            }
+        });
+        // Destroy the editor instance when element is removed
+        ko.utils.domNodeDisposal.addDisposeCallback(element, function() {
+            editor.destroy();
+        });
+    },
+    update: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
+        var value = ko.utils.unwrapObservable(valueAccessor());
+        // Handle programmatic updates to the observable,
+        // also makes sure it doesn't update it if it's the same.
+        // Otherwise, it will reload the instance, causing the cursor to jump.
+        var content = editor.getValue();
+        if (content !== value) {
+            editor.setValue(value);
+            editor.gotoLine( 0 );
+        }
+    }
+};
+
 // Resize editor window based on browser window.innerHeight
 function resizeAce() {
     // http://stackoverflow.com/questions/11584061/
@@ -6,24 +40,28 @@ function resizeAce() {
     editor.resize();
 }
 
-
 // Additional syntax highlight logic
 function getCustomMode(filename) {
     var _config = (/^(web|app).config$/i);
     var _csproj = (/.(cs|vb)proj$/i);
+    var _xdt = (/.xdt$/i);
+    var _aspnet = (/.(cshtml|asp|aspx)$/i);
     var syntax_mode = 'ace/mode/text';
     if (
         filename.match(_config) ||
-        filename.match(_csproj)
+        filename.match(_csproj) ||
+        filename.match(_xdt)
        )
     {
         syntax_mode = 'ace/mode/xml';
     }
+    if (filename.match(_aspnet)) {
+        syntax_mode = 'ace/mode/csharp';
+    }
     return syntax_mode;
 }
 
-
-// Init Ace
+// Act II - The Editor Awakens
 var editor = ace.edit("editor");
 editor.setTheme("ace/theme/github");
 editor.getSession().setTabSize(4);
@@ -34,12 +72,20 @@ editor.setOptions({
     "fontSize": 14
 });
 
-
 // Show a red bar if content has changed
-editor.on('change', function() {
-   $('#statusbar').css('border-left-color', '#f14');
+var contentHasChanged = false;
+editor.on('change', function () {
+    // (Attempt to) separate user change from programatical
+    // https://github.com/ajaxorg/ace/issues/503
+    if (editor.curOp && editor.curOp.command.name) {
+        if (contentHasChanged) {
+            return;
+        }
+        $('#statusbar').addClass('statusbar-red');
+        // Let's be nice to jQuery and only .addClass() on first change
+        contentHasChanged = true;
+    }
 });
-
 
 // Hook the little pencil glyph and apply Ace syntax mode based on file extension
 $('#fileList').on('click', '.glyphicon-pencil', function () {
@@ -50,7 +96,7 @@ $('#fileList').on('click', '.glyphicon-pencil', function () {
         }
         catch (e) {
             if (typeof console == 'object') {
-                console.log('Can\'t get filename. ' + e);
+                console.log('Can not get filename. ' + e);
             }
         }
         finally {
