6eb4ac9d65b4293d820f2c5a13d8c0117ab38553
davidebbo <david.ebbo@microsoft.com>
2015-08-31 07:06:18 -0700

    Use msbuild from VS 2015 now that we have the newer F#

--- Kudu.Core/Infrastructure/PathUtility.cs
+++ Kudu.Core/Infrastructure/PathUtility.cs
@@ -74,8 +74,8 @@ namespace Kudu.Core.Infrastructure
 
         internal static string ResolveMSBuildPath()
         {
-            string windir = SystemEnvironment.GetFolderPath(SystemEnvironment.SpecialFolder.Windows);
-            return Path.Combine(windir, @"Microsoft.NET", "Framework", "v4.0.30319", "MSBuild.exe");
+            string programFiles = SystemEnvironment.GetFolderPath(SystemEnvironment.SpecialFolder.ProgramFilesX86);
+            return Path.Combine(programFiles, @"MSBuild", "14.0", "Bin", "MSBuild.exe");
         }
 
         internal static string ResolveVsTestPath()
