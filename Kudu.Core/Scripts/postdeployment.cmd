5b438052092636cc063e00c30e445075175d74b3
Xiaomin Wu <xiaowu@microsoft.com>
2015-12-17 15:53:50 -0800

    Fixes #1833, Support for Powershell execution in Post deployment actions.

--- Kudu.Core/Scripts/postdeployment.cmd
+++ Kudu.Core/Scripts/postdeployment.cmd
@@ -3,10 +3,15 @@
 setlocal enabledelayedexpansion
 
 IF EXIST %POST_DEPLOYMENT_ACTIONS_DIR% (
-  FOR /F %%A IN ('dir /s /b %POST_DEPLOYMENT_ACTIONS_DIR%\*.cmd %POST_DEPLOYMENT_ACTIONS_DIR%\*.bat') DO (
+  FOR /F %%A IN ('dir /s /b %POST_DEPLOYMENT_ACTIONS_DIR%\*.cmd %POST_DEPLOYMENT_ACTIONS_DIR%\*.bat %POST_DEPLOYMENT_ACTIONS_DIR%\*.ps1') DO (
     set CURRENT_SCRIPT_FILE=%%A
     echo Executing - '!CURRENT_SCRIPT_FILE!'
-    call !CURRENT_SCRIPT_FILE!
+    :: Get last four chars and check if file end with '.ps1' (powershell file)
+    IF /I "!CURRENT_SCRIPT_FILE:~-4!"==".ps1" (
+        call PowerShell -NoProfile -NoLogo -ExecutionPolicy RemoteSigned "!CURRENT_SCRIPT_FILE!"
+    ) else (
+        call "!CURRENT_SCRIPT_FILE!"
+    )
     IF !ERRORLEVEL! NEQ 0 goto error
     echo '!CURRENT_SCRIPT_FILE!' executed successfully
   )
