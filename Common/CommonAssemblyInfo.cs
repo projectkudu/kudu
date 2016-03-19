9a3da17d9452163d4f60568a9974e79b32e2b465
davidebbo <david.ebbo@microsoft.com>
2016-03-18 19:45:19 -0700

    Change version to 53

--- Common/CommonAssemblyInfo.cs
+++ Common/CommonAssemblyInfo.cs
@@ -8,8 +8,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 
 // If you change this version, make sure to change Build\build.proj accordingly
-[assembly: AssemblyVersion("52.0.0.0")]
-[assembly: AssemblyFileVersion("52.0.0.0")]
+[assembly: AssemblyVersion("53.0.0.0")]
+[assembly: AssemblyFileVersion("53.0.0.0")]
 
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(false)]
