a9703a4ec879e66f5b32a99f68ca68c05feeb0ee
Jozef Izso <jozef.izso@gmail.com>
2016-03-29 18:10:33 +0200

    Add copy to clipboard button to Git URL textboxes.

---
+++ Kudu.Web/Scripts/Site.js
@@ -0,0 +1,35 @@
+﻿
+$(function() {
+    var clipboard = new Clipboard('.btn-clipboard');
+
+    clipboard.on('success', function (e) {
+        e.clearSelection();
+
+        $(e.trigger).tooltip({ title: 'Copied!' }).tooltip('show');
+    });
+
+    clipboard.on('error', function (e) {
+        console.error('Action:', e.action);
+        console.error('Trigger:', e.trigger);
+
+        $(e.trigger).tooltip({ title: fallbackMessage(e.action) }).tooltip('show');
+    });
+
+    $('#clone-url').click(function () {
+        this.select();
+    });
+});
+
+function fallbackMessage(action) {
+    var actionMsg = '';
+    var actionKey = (action === 'cut' ? 'X' : 'C');
+
+    if (/Mac/i.test(navigator.userAgent)) {
+        actionMsg = 'Press ⌘-' + actionKey + ' to ' + action;
+    }
+    else {
+        actionMsg = 'Press Ctrl-' + actionKey + ' to ' + action;
+    }
+
+    return actionMsg;
+}
