44570100217a560563ce577cebc8be8fba470a96
David Ebbo <david.ebbo@gmail.com>
2016-05-30 09:44:47 -0700

    Also fix StartProfileAsync_InvalidProcess

--- Kudu.Core.Test/ProcessApiFacts.cs
+++ Kudu.Core.Test/ProcessApiFacts.cs
@@ -261,7 +261,7 @@ namespace Kudu.Core.Test
             controller.Object.Request = new HttpRequestMessage();
 
             // Test
-            var ex = await Assert.ThrowsAsync<HttpResponseException>(() => controller.Object.StartProfileAsync(100));
+            var ex = await Assert.ThrowsAsync<HttpResponseException>(() => controller.Object.StartProfileAsync(-2));
 
             // Assert
             Assert.Equal(HttpStatusCode.NotFound, ex.Response.StatusCode);
