2ca3ddc14b7f352a08a0f89cd64d3854bf4e2897
David Ebbo <david.ebbo@gmail.com>
2016-06-23 11:09:06 -0700

    Bump version to 56

--- Common/CommonAssemblyInfo.cs
+++ Common/CommonAssemblyInfo.cs
@@ -8,8 +8,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 
 // If you change this version, make sure to change Build\build.proj accordingly
-[assembly: AssemblyVersion("55.0.0.0")]
-[assembly: AssemblyFileVersion("55.0.0.0")]
+[assembly: AssemblyVersion("56.0.0.0")]
+[assembly: AssemblyFileVersion("56.0.0.0")]
 
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(false)]
