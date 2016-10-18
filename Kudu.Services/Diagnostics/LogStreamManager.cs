aa7e94ed8f454212fb9b30f3803f1e95d9286f50
Suwath Ch <suwatch@microsoft.com>
2016-10-17 17:01:09 -0700

    retry on logstream update setting failure

--- Kudu.Services/Diagnostics/LogStreamManager.cs
+++ Kudu.Services/Diagnostics/LogStreamManager.cs
@@ -93,8 +93,12 @@ namespace Kudu.Services.Performance
             {
                 _operationLock.LockOperation(() =>
                 {
-                    var diagnostics = new DiagnosticsSettingsManager(Path.Combine(_environment.DiagnosticsPath, Constants.SettingsJsonFile), _tracer);
-                    diagnostics.UpdateSetting(AzureDriveEnabledKey, true);
+                    // retry 5 times with 1 sec interval
+                    OperationManager.Attempt(() =>
+                    {
+                        var diagnostics = new DiagnosticsSettingsManager(Path.Combine(_environment.DiagnosticsPath, Constants.SettingsJsonFile), _tracer);
+                        diagnostics.UpdateSetting(AzureDriveEnabledKey, true);
+                    }, retries: 5, delayBeforeRetry: 1000);
                 }, "Updating diagnostics setting", TimeSpan.FromSeconds(30));
             }
 
