7e5bbea57f03a72abf79b09b467b22869702c403
James Christianson <jamchri@microsoft.com>
2017-03-31 14:13:11 -0700

    Add endpoint for docker hooks (linux only)

---
+++ Kudu.Services/Docker/DockerController.cs
@@ -0,0 +1,46 @@
+﻿using Kudu.Core.Helpers;
+using Kudu.Core.Tracing;
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Kudu.Services.Docker
+{
+    public class DockerController : ApiController
+    {
+        const string TimestampDirectory = "/home/config";
+        ITraceFactory _traceFactory;
+
+        public DockerController(ITraceFactory traceFactory)
+        {
+            _traceFactory = traceFactory;
+        }
+
+        [HttpPost]
+        public HttpResponseMessage ReceiveHook()
+        {
+            if (OSDetector.IsOnWindows())
+            {
+                return Request.CreateResponse(HttpStatusCode.NotFound);
+            }
+
+            var tracer = _traceFactory.GetTracer();
+            using (tracer.Step("Docker.SetDockerTimestamp"))
+            {
+                string timestampPath = Path.Combine(TimestampDirectory, "dockerTimestamp.txt");
+                try
+                {
+                    File.WriteAllText(timestampPath, DateTime.UtcNow.ToString());
+                }
+                catch (Exception e)
+                {
+                    return Request.CreateResponse(HttpStatusCode.InternalServerError, e.Message);
+                }
+
+                return Request.CreateResponse(HttpStatusCode.OK);
+            }
+        }
+    }
+}
