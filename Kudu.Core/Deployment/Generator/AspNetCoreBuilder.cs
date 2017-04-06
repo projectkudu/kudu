06c4b6b5ea52ca9a24fc1531eaf1ddf7c357af47
shucai <shucai@microsoft.com>
2017-04-06 15:25:53 -0700

    refine dotnet core project type, facilitate yank process

--- Kudu.Core/Deployment/Generator/AspNetCoreBuilder.cs
+++ Kudu.Core/Deployment/Generator/AspNetCoreBuilder.cs
@@ -1,6 +1,5 @@
-﻿using Kudu.Contracts.Settings;
-using Kudu.Core.SourceControl;
-using System.IO;
+﻿using System;
+using Kudu.Contracts.Settings;
 
 namespace Kudu.Core.Deployment.Generator
 {
@@ -8,12 +7,25 @@ namespace Kudu.Core.Deployment.Generator
     {
         private readonly string _projectPath;
         private readonly string _solutionPath;
+        private readonly string _version;
 
         public AspNetCoreBuilder(IEnvironment environment, IDeploymentSettingsManager settings, IBuildPropertyProvider propertyProvider, string sourcePath, string projectPath, string solutionPath = null)
             : base(environment, settings, propertyProvider, sourcePath)
         {
-            _projectPath = projectPath;
+            _projectPath = projectPath; // either xproj, csproj or project.json
             _solutionPath = solutionPath;
+            if (_projectPath.EndsWith(".csproj", StringComparison.OrdinalIgnoreCase))
+            {
+                _version = "csproj";
+            }
+            else if (_projectPath.EndsWith(".xproj", StringComparison.OrdinalIgnoreCase))
+            {
+                _version = "xproj";
+            }
+            else
+            {
+                _version = "project.json";
+            }
         }
 
         protected override string ScriptGeneratorCommandArguments
@@ -23,20 +35,18 @@ namespace Kudu.Core.Deployment.Generator
 
                 if (string.IsNullOrEmpty(_solutionPath))
                 {
-                    return $"--aspNetCore \"{_projectPath}\""; 
-                    // projectfile is either project.json for preview2, ***.csproj for preview3
+                    return $"--aspNetCore \"{_projectPath}\"";
                 }
                 else
                 {
                     return $"--aspNetCore \"{_projectPath}\" --solutionFile \"{_solutionPath}\"";
-                    // if we have a solution file, projectfile is either ***.xproj for preview2, ***.csproj for preview3
                 }
             }
         }
 
         public override string ProjectType
         {
-            get { return "ASP.NET Core"; }
+            get { return $"ASP.NET Core {_version}"; }
         }
     }
 }
