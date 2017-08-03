2bd7755fba480a487117bc6283288adeb5b108d5
David Ebbo <david.ebbo@gmail.com>
2017-08-02 18:39:57 -0700

    Version 64

--- Common/CommonAssemblyInfo.cs
+++ Common/CommonAssemblyInfo.cs
@@ -8,8 +8,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 
 // If you change this version, make sure to change Build\build.proj accordingly
-[assembly: AssemblyVersion("63.0.0.0")]
-[assembly: AssemblyFileVersion("63.0.0.0")]
+[assembly: AssemblyVersion("64.0.0.0")]
+[assembly: AssemblyFileVersion("64.0.0.0")]
 
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(false)]
