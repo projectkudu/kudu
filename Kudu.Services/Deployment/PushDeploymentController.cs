f90fcdee18172a85634b67acaa60b52d1256c968
Nick Walker <nickwalk@microsoft.com>
2017-09-21 19:08:33 -0700

    Update comments

--- Kudu.Services/Deployment/PushDeploymentController.cs
+++ Kudu.Services/Deployment/PushDeploymentController.cs
@@ -71,7 +71,10 @@ namespace Kudu.Services.Deployment
             {
 
                 // TODO should we reject requests if SCM is enabled?
-                // (like FetchHandler line 115)
+                // FetchHandler does on line 115, but explicitly calls out that that
+                // check is skipped for git pushes and GenericHandler/DropBoxHandler because
+                // they "can only be done by user action, we loosely allow that and
+                // assume users know what they are doing"
 
                 try
                 {
@@ -84,15 +87,13 @@ namespace Kudu.Services.Deployment
                             return Request.CreateErrorResponse(HttpStatusCode.Conflict, Resources.Error_AutoSwapDeploymentOngoing);
                         }
 
-                        // TODO create temp deployment here?
-
-                        // TODO: For repository, should we do an EnsureRepository check, or just new it up?
-                        // Not sure if we want to fail with a repository mismatch error if other work already done?
-                        //var repository = _repositoryFactory.EnsureRepository(RepositoryType.None);
+                        // TODO Create temp deployment here
 
                         // TODO What should the path be? 
                         // should repositoryFactory take care of it? What should it be based on?
                         // Should it vary between deployments?
+                        // Should it be the repository folder (don't see the point of that, when we could put it
+                        // in a temp folder on the local drive for speed)
                         // Should it always be a new folder? If not, do we clean it out first?
                         // Also make sure that it's on the local drive and not on the user drive for speed.
                         // size concerns?
@@ -100,31 +101,26 @@ namespace Kudu.Services.Deployment
                         // Ugly hack here for now
                         var extractPath = Path.Combine(_environment.TempPath, Path.GetRandomFileName());
 
-                        // TODO Not sure if inside the lock is the right place to extract the stream.
-                        // TODO Would we rather write it to disk before expanding? Should probably always do that 
-                        // for async deploys, as the extract may take a while.
+                        // TODO: For repository, should we do an EnsureRepository check, or just new it up?
+                        // Not sure if we want to fail with a repository mismatch error if other work already done?
+                        // TODO note that Fetch *always* does EnsureRepository, so what's happening here is not compatible
+                        // with that setup as-is.
+                        var repository = _repositoryFactory.GetZipDeployRepository(extractPath);
 
+                        // TODO For async deploys, write the stream to a file and extract later
                         using (var stream = await Request.Content.ReadAsStreamAsync())
                         {
                             var zipArchive = new ZipArchive(stream, ZipArchiveMode.Read);
                             zipArchive.Extract(extractPath);
                         }
 
-                        // TODO We don't need to bother constructing a DeploymentInfo here but
-                        // saving this for reference when doing fetch later
-                        //var deployInfo = new DeploymentInfo();
-                        //deployInfo.RepositoryType = RepositoryType.None;
-                        //deployInfo.Deployer = "Zip-Push";
-                        //deployInfo.TargetChangeset = DeploymentManager.CreateTemporaryChangeSet(
-                        //    message: "Deploying from zip file");
-
                         // TODO Doing changeset this way for consistency with future fetch deploy implementation,
                         // which requires that the repository have changeset that can be retrieved with GetChangeSet.
                         // The below is what we do for OneDrive and Dropbox during a fetch.
                         // Not sure if it's the right thing to do.
-                        var repository = _repositoryFactory.GetZipDeployRepository(extractPath);
                         repository.Commit("Extracting pushed zip file", authorName: null, emailAddress: null);
                         var changeSet = repository.GetChangeSet("HEAD");
+
                         var deployer = "Zip-Push";
                         await _deploymentManager.DeployAsync(repository, changeSet, deployer, clean: false, needFileUpdate: false);
 
