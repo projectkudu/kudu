fcfd3f6a2169d89532883d6b0914dd361f9595cb
shucai <shucai@microsoft.com>
2017-10-05 14:05:08 -0700

    remove unnecessary test

--- Kudu.Core.Test/Functions/FunctionSecretTests.cs
+++ Kudu.Core.Test/Functions/FunctionSecretTests.cs
@@ -9,20 +9,16 @@ namespace Kudu.Core.Test.Functions
         [Fact]
         public void DecryptEncryptedKeyTests()
         {
-            Tuple<string, string>[] keyPairs = SecurityUtility.GenerateSecretStringsKeyPair(2); // get a key pair <unencrypted, encrypted>
-            string firstUnencryptedKey = keyPairs[0].Item1;
-            string firstEncryptedKey = keyPairs[0].Item2;
-            string secondEncryptedKey = keyPairs[1].Item2;
+            // get a key pair <unencrypted, encrypted>
+            Tuple<string, string>[] keyPairs = SecurityUtility.GenerateSecretStringsKeyPair(1);
+            string unencryptedKey = keyPairs[0].Item1;
+            string encryptedKey = keyPairs[0].Item2;
 
-            Assert.Equal(firstUnencryptedKey, SecurityUtility.DecryptSecretString(firstEncryptedKey));
-            Assert.NotEqual(firstUnencryptedKey, SecurityUtility.DecryptSecretString(secondEncryptedKey)); // decryption success, but value is wrong
-            var exception = Assert.Throws<FormatException>(() => SecurityUtility.DecryptSecretString(firstUnencryptedKey)); // try to decrypt an unencrypted key will throw exception
-            // wrapper around the following since we treate CryptographicException as 500 server error
-            // Exceptions:
-            //   T:System.Security.Cryptography.CryptographicException:
-            //     Thrown if protectedData is invalid or malformed.
-            // public static string Unprotect(this IDataProtector protector, string protectedData);
-            Assert.Equal($"unable to decrypt {firstUnencryptedKey}, the key is either invalid or malformed", exception.Message);
+            Assert.Equal(unencryptedKey, SecurityUtility.DecryptSecretString(encryptedKey));
+            var exception = Assert.Throws<FormatException>(() => SecurityUtility.DecryptSecretString(unencryptedKey)); // try to decrypt an unencrypted key will throw exception
+
+            // map CryptographicException to FormatException so that HTTP return 400 instead of 500
+            Assert.Equal($"unable to decrypt {unencryptedKey}, the key is either invalid or malformed", exception.Message);
         }
     }
 }
