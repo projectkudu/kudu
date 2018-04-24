193e17960174ef2ec5d897062580487b8d18bbe5
lingtoh <lingtoh@gmail.com>
2018-04-24 10:46:50 -0700

    Durable Functions - Support reading storage connection string in host.json

--- Common/Constants.cs
+++ Common/Constants.cs
@@ -120,6 +120,7 @@ namespace Kudu
         public const string DynamicSku = "Dynamic";
         public const string AzureWebJobsSecretStorageType = "AzureWebJobsSecretStorageType";
         public const string HubName = "HubName";
+        public const string DurableTaskStorageConnection = "connection";
         public const string DurableTask = "durableTask";
         public const string SitePackages = "SitePackages";
         public const string SiteVersionTxt = "siteversion.txt";
