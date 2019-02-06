a6772c46d4684366514d668322e6d279527291ad
Suwath Ch <suwatch@microsoft.com>
2019-02-06 10:03:22 -0800

    update version to 80

--- Common/CommonAssemblyInfo.cs
+++ Common/CommonAssemblyInfo.cs
@@ -8,8 +8,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 
 // If you change this version, make sure to change Build\build.proj accordingly
-[assembly: AssemblyVersion("79.0.0.0")]
-[assembly: AssemblyFileVersion("79.0.0.0")]
+[assembly: AssemblyVersion("80.0.0.0")]
+[assembly: AssemblyFileVersion("80.0.0.0")]
 
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(false)]
