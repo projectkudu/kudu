7ab087f0367353c10834bb3479d4d0cad7f96da9
Nick Schonning <nschonni@gmail.com>
2019-05-16 00:49:30 -0400

    typo: divid -> divide

--- Kudu.Contracts/Functions/FunctionTestData.cs
+++ Kudu.Contracts/Functions/FunctionTestData.cs
@@ -3,7 +3,7 @@
     public class FunctionTestData
     {
         // ARM has a limit of 8 MB -> 8388608 bytes
-        // divid by 2 to limit the over all size of test data to half of arm requirement to be safe.
+        // divide by 2 to limit the over all size of test data to half of arm requirement to be safe.
         public const long PackageMaxSizeInBytes = 8388608 / 2;
 
         public long BytesLeftInPackage { get; set; } = PackageMaxSizeInBytes;
