bc2629a2097cac17bbd0a8cfc0f953550246abef
Suwath Ch <suwatch@microsoft.com>
2020-02-04 16:14:45 -0800

    add support for ScmHostingConfigurations

---
+++ Kudu.Core.Test/Settings/ScmHostingConfigurationsTests.cs
@@ -0,0 +1,30 @@
+﻿using Kudu.Core.Settings;
+using Xunit;
+
+namespace Kudu.Core.Test.Settings
+{
+    public class ScmHostingConfigurationsTests
+    {
+        [Theory]
+        [InlineData("a=b,foo=bar,c=d", 3, "foo", "bar")]
+        [InlineData("a=b,foo=bar,c=d", 3, "FoO", "bar")]
+        [InlineData("foo=bar,c=d", 2, "c", "d")]
+        [InlineData("a=,foo=bar,c=d", 2, "a", null)]
+        public void ScmHostingConfigurationsParseTests(string settings, int count, string key, string value)
+        {
+            var dict = ScmHostingConfigurations.Parse(settings);
+            Assert.Equal(count, dict.Count);
+            if (!string.IsNullOrEmpty(key))
+            {
+                if (!string.IsNullOrEmpty(value))
+                {
+                    Assert.Equal(value, dict[key]);
+                }
+                else
+                {
+                    Assert.False(dict.ContainsKey(key));
+                }
+            }
+        }
+    }
+}
---
+++ Kudu.Core/Settings/ScmHostingConfigurations.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Kudu.Contracts.Tracing;
+using Kudu.Core.Infrastructure;
+using Kudu.Core.Tracing;
+
+namespace Kudu.Core.Settings
+{
+    public static class ScmHostingConfigurations
+    {
+        private readonly static string _configsFile = 
+            System.Environment.ExpandEnvironmentVariables(@"%ProgramFiles(x86)%\SiteExtensions\kudu\ScmHostingConfigurations.txt");
+
+        private static Dictionary<string, string> _configs;
+        private static DateTime _configsTTL;
+
+        public static string GetValue(string key, string defaultValue = null, ITracer tracer = null)
+        {
+            var configs = _configs;
+            if (configs == null || DateTime.UtcNow > _configsTTL)
+            {
+                _configsTTL = DateTime.UtcNow.AddMinutes(10);
+
+                try
+                {
+                    var settings = FileSystemHelpers.FileExists(_configsFile) 
+                        ? FileSystemHelpers.ReadAllText(_configsFile) 
+                        : null;
+                    if (tracer != null)
+                    {
+                        tracer.Trace($"Update ScmHostingConfigurations: {settings}");
+                    }
+
+                    configs = Parse(settings);
+                    _configs = configs;
+                }
+                catch (Exception ex)
+                {
+                    if (tracer != null)
+                    {
+                        tracer.TraceError(ex);
+                    }
+                }
+            }
+
+            return (configs == null || !configs.TryGetValue(key, out string value)) ? defaultValue : value;
+        }
+
+        public static Dictionary<string, string> Parse(string settings)
+        {
+            return string.IsNullOrEmpty(settings)
+                ? new Dictionary<string, string>()
+                : settings
+                    .Split(new[] { ',', ';' }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(s => s.Split(new[] { '=' }, StringSplitOptions.RemoveEmptyEntries))
+                    .Where(a => a.Length == 2)
+                    .ToDictionary(a => a[0], a => a[1], StringComparer.OrdinalIgnoreCase);
+        }
+    }
+}
