db7f8ceaea3ac951342abfc25cd677a6d0802371
Jonathan Kleinfeld <jokleinf@microsoft.com>
2020-05-29 14:16:55 -0700

    Persist new ID through changeset so it carries over to later deployment steps

--- Kudu.Core/Deployment/DeploymentManager.cs
+++ Kudu.Core/Deployment/DeploymentManager.cs
@@ -181,7 +181,21 @@ namespace Kudu.Core.Deployment
                     }
                 }
 
-                string id = deploymentInfo.FixedDeploymentId ?? changeSet.Id;
+                if (!string.IsNullOrEmpty(deploymentInfo.FixedDeploymentId))
+                {
+                    changeSet = new ChangeSet(
+                        id: deploymentInfo.FixedDeploymentId,
+                        authorName: changeSet.AuthorName,
+                        authorEmail: changeSet.AuthorEmail,
+                        message: changeSet.Message,
+                        timestamp: changeSet.Timestamp)
+                    {
+                        IsTemporary = changeSet.IsTemporary,
+                        IsReadOnly = changeSet.IsReadOnly
+                    };
+                }
+
+                string id = changeSet.Id;
                 IDeploymentStatusFile statusFile = null;
                 try
                 {
