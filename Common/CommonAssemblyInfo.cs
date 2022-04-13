af622d28a1fd5651e5db806c7ac72091e3d6112c
Suwath Ch <suwatch@microsoft.com>
2022-04-12 17:07:09 -0700

    update Kudu version to 97

--- Common/CommonAssemblyInfo.cs
+++ Common/CommonAssemblyInfo.cs
@@ -8,8 +8,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 
 // If you change this version, make sure to change Build\build.proj accordingly
-[assembly: AssemblyVersion("96.0.0.0")]
-[assembly: AssemblyFileVersion("96.0.0.0")]
+[assembly: AssemblyVersion("97.0.0.0")]
+[assembly: AssemblyFileVersion("97.0.0.0")]
 
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(false)]
