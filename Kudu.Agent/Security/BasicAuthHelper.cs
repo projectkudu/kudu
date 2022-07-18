de978132c9f19fc0c5560abb6efc9677f7e5f325
Benjamin Hardin <t-behardin@microsoft.com>
2022-07-18 15:11:24 -0700

    Adds authorization checking using Logon function

--- Kudu.Agent/Security/BasicAuthHelper.cs
+++ Kudu.Agent/Security/BasicAuthHelper.cs
@@ -6,13 +6,21 @@ using System.Net;
 using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.Security;
+using System.Security.Principal;
+using Microsoft.Win32.SafeHandles;
 
 namespace Kudu.Agent.Security
-{
+{   
     public class BasicAuthHelper : ControllerBase
     {
 
         private readonly RequestDelegate _requestDelegate;
+
+        [DllImport("advapi32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+        public static extern bool LogonUser(String lpszUsername, String lpszDomain, String lpszPassword,
+            int dwLogonType, int dwLogonProvider, out SafeAccessTokenHandle phToken);
 
         public BasicAuthHelper(RequestDelegate requestDelegate)
         {
@@ -21,36 +29,42 @@ namespace Kudu.Agent.Security
 
         public async Task Invoke(HttpContext context)
         {
-            try
-            {
-                var authHeader = AuthenticationHeaderValue.Parse(context.Request.Headers["Authorization"]);
-                var credentialBytes = Convert.FromBase64String(authHeader.Parameter);
-                var credentials = Encoding.UTF8.GetString(credentialBytes).Split(':', 2);
-                var username = credentials[0];
-                var password = credentials[1];
+            var authHeader = AuthenticationHeaderValue.Parse(context.Request.Headers["Authorization"]);
+            var credentialBytes = Convert.FromBase64String(authHeader.Parameter);
+            var credentials = Encoding.UTF8.GetString(credentialBytes).Split(':', 2);
+            var username = credentials[0];
+            var password = credentials[1];
 
-                authenticateUser(username, password);
-                
-            }
-            catch
-            {
-                throw new UnauthorizedAccessException();
-            }
+            const int LOGON32_PROVIDER_DEFAULT = 0;
+            //This parameter causes LogonUser to create a primary token.   
+            const int LOGON32_LOGON_INTERACTIVE = 2;
 
-            await _requestDelegate(context);
-        }
-
-        public IActionResult authenticateUser(string username, string password)
-        {
-            if (String.Equals(username, "test") && String.Equals(password, "test2"))
+            // Call LogonUser to obtain a handle to an access token.   
+            SafeAccessTokenHandle safeAccessTokenHandle;
+            bool success = LogonUser(username, "", password,
+                LOGON32_LOGON_INTERACTIVE, LOGON32_PROVIDER_DEFAULT,
+                out safeAccessTokenHandle);
+            if (success)
             {
-                
-                return Ok();
+                // may need to do something here to say that we are now verified!
+                await _requestDelegate(context);
             }
             else
             {
-                return BadRequest(new { message = "Username or password is incorrect."});
+                context.Response.StatusCode = 401; //UnAuthorized
+                await context.Response.WriteAsync("Username or password is incorrect.");
             }
+            /*else
+            {
+                throw new Unauthorized(new { message = "Username or password is incorrect." });
+                //return BadRequest(new { message = "Username or password is incorrect."});
+            }*/
         }
+
+        /*public IActionResult authenticateUser(string username, string password)
+        {
+
+            
+        }*/
     }
 }
